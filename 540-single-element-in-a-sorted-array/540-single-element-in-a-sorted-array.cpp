d72d46eed38a250392b723ee4107628edc0f2f8d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-08 19:12:46 +0530

    Time: 31 ms (8.22%), Space: 22.3 MB (12.39%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -13,25 +13,12 @@ public:
     //TC:- O(logN) SC:-O(1)
       int singleNonDuplicate(vector<int>& nums){
           if(nums.size()==1) return nums[0];
-          int low=0,high=nums.size()-1;
+          int low=0,high=nums.size()-2;
           while(low<=high)
           {
-             int mid=low+(high-low)/2;
-             if(mid%2)
-             {
-                 //even-odd pair->answer lies in the right part of the array
-              if(nums[mid-1]==nums[mid])
-                  low=mid+1;
-              else//even-odd pair violated-> answer lies in the left part
-                  high=mid-1;
-             }else
-             {
-                 //even odd pair->we're on the left part but answer lies in the right part 
-                 if(nums[mid]==nums[mid+1])
-                      low=mid+1;
-                 else //same pair violated->answer lies in the left part
-                     high=mid-1;
-             }
+             int mid=(low+high)>>1;
+             if(nums[mid]==nums[mid^1]) low=mid+1;
+             else high=mid-1;
           }
           return nums[low];
       }
