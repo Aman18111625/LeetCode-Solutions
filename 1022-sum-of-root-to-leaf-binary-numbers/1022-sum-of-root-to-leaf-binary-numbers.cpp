8711677a3bda5cb0ceb4787422a32c2599c5614f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-11 11:11:07 +0530

    Time: 4 ms (87.29%), Space: 16.9 MB (29.55%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -10,6 +10,7 @@
  * };
  */
 class Solution {
+ /*
    int  helper(TreeNode* root, int curr) {
         if(!root) return 0;
         curr = (curr<<1)|(root->val);
@@ -19,5 +20,29 @@ class Solution {
 public:
     int sumRootToLeaf(TreeNode* root) {
         return helper(root,0);
+    }
+    */
+    public:
+     int sumRootToLeaf(TreeNode* root) {
+        if(!root) return 0;
+        int ans = 0;
+        stack<pair<TreeNode*,int>> st;
+        st.push({root,0});
+        while(!st.empty()) {
+            auto node= st.top(); st.pop();
+            TreeNode* cur = node.first;
+            int cur_val = node.second;
+            // Equivalent to left shift by 1 and then ORing by cur->val.
+            cur_val = cur_val*2 + cur->val;
+            
+            if(!cur->left && !cur->right)
+                ans += cur_val;
+            
+            if(cur->left) st.push({cur->left, cur_val});
+            if(cur->right) st.push({cur->right, cur_val});
+        }
+        
+        return ans;
+        
     }
 };
