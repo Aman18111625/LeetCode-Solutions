627901f3264a87c3542f0614d737434c13bce9a5
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-13 22:27:39 +0530

    Time: 32 ms (37.21%), Space: 12.7 MB (65.79%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
+    /*
     bool repeatedSubstringPattern(string s) {
         string rot=s+s;
         rot.pop_back();
         rot=rot.substr(1);
         if(rot.find(s)!=string::npos) return true;
         return false;
+    }*/
+     bool repeatedSubstringPattern(string s) {
+        int n=s.length();
+        vector<int> lps(n,0);
+        int i=1, j=0;
+        for(i=1;i<n;)
+        {
+            if(s[i]==s[j])
+            {
+                lps[i++]=++j;
+            }
+            else
+            {
+                if(j)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    lps[i++]=0;
+                }
+            }
+        }
+        return lps[n-1] && (lps[n-1]%(n-lps[n-1])==0);
     }
 };
