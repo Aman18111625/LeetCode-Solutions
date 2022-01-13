a75e6da42d4cfaab2332beb45e9cfebab5eef54d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-13 21:47:51 +0530

    Added solution - LeetHub

--- Pattern searching - GFG/pattern-searching.cpp
+++ Pattern searching - GFG/pattern-searching.cpp
@@ -16,8 +16,20 @@ text: pattern needs to searched.
 bool searchPattern(string str, string pat)
 {
     // your code here
+    /*
     if(str.find(pat)==string::npos) return false;
-    return true;
+    return true;*/
+    int n=str.length(),m=pat.size();
+    for(int i=0;i<=(n-m);i++)
+    { 
+        int j;
+        for(j=0;j<m;j++)
+        {
+            if(str[i+j]!=pat[j]) break;
+        }
+        if(j==m) return true;
+    }
+    return false;
 }
 
 
