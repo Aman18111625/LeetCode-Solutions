9d75c23eb5bce17687b2fd833d2611febbde24c1
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-13 12:31:45 +0530

    Time: 691 ms (20.99%), Space: 89.7 MB (92.23%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -3,7 +3,7 @@ public:
     int findMinArrowShots(vector<vector<int>>& p) {
          sort(p.begin(),p.end());//sort according to start time
         int n=p.size(),lastEnd=p[0][1];
-        int ans=0;
+        int ans=1;//we need to atleast one arrow to burst first ballons
         for(auto &curr : p)
         {
         if(lastEnd>=curr[0])//if interval is intersecting then update lastEnd with minimum of them
@@ -15,7 +15,6 @@ public:
                 lastEnd=curr[1];
             }
         }
-        ans++;//doing this because we're unable to check for last interval
         return ans;
     }
 };
