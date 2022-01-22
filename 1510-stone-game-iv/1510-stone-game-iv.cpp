92c86fcf5e2358c1f7d6f211d5b9627c08fefa52
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-22 22:17:20 +0530

    Time: 113 ms (40.94%), Space: 12.1 MB (29.53%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,19 +1,19 @@
 class Solution {
+  int dp[100001];
+  bool helper(int n)
+  {
+    if(n<=0) return false;
+    if(dp[n]!=-1) return dp[n];
+    for(int i=1;i*i<=n;i++)
+    {
+      if(!helper(n-i*i)) return dp[n]=true;
+    }
+    return dp[n]=false;
+  }
 public:
-    vector<int> dp;
-    Solution() : dp(100001,-1) {}
     bool winnerSquareGame(int n) {
-          if(n==0) return false;
-          if(dp[n]!=-1) return dp[n];
-          bool ans=false;
-          for(int i=1;i*i<=n;i++)
-          {
-            if(!winnerSquareGame(n-i*i)) 
-            {
-              ans=true; break;
-            }
-          }
-      return dp[n]=ans;
+        memset(dp,-1,sizeof(dp));
+        return helper(n);
     }
 };
  
