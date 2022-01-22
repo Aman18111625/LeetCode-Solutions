b22d7db4b80253c6399728f2ca47947b09061570
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-23 00:32:12 +0530

    Time: 44 ms (21.16%), Space: 13.4 MB (73.26%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,5 +1,50 @@
 class Solution {
-public:
+  public:
+   bool topo(int start,vector<int>adj[], int n,bool *vis, bool *s)
+    {
+        vis[start]=true;
+        s[start]=true;
+        for(int v: adj[start])
+        {
+            if(!vis[v])
+            {
+                if(topo(v,adj,n,vis,s))
+                return true;
+            }
+            else if(s[v])
+                return true;
+        }
+        s[start]=false;
+        return false;
+    }
+    
+    
+    bool canFinish(int courses, vector<vector<int>>& prerequisites) 
+    {
+        int n =courses;
+        vector<int>adj[n];
+        for(int i=0;i<prerequisites.size();i++)
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        
+        bool s[n], vis[n];
+        for(int i=0;i<n;i++) 
+        {
+            vis[i]=false;
+            s[i]=false;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(topo(i,adj,n,vis,s)) //if cycle detected then return false
+                return false;
+        }
+        
+        return true;
+        
+    }
+    
+  
+  /*BFS
     bool canFinish(int num, vector<vector<int>>&a) {
          int n=a.size();
          int cnt=0;
@@ -30,7 +75,8 @@ public:
         }
         
         return cnt==num;
-    }
+    }*/
+  
 };
  
       
