65231fa7a4759ab58db6fd406d46d6fd46b85e6d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-25 11:23:27 +0530

    Time: 13 ms (15.59%), Space: 13.8 MB (12.26%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -22,7 +22,7 @@ public:
   /*
   thought process:->
   first of all we'll traverse from left to right ,that'll give us maximum of left t0 right direction. Now we'll do same for right to left direction .In traverse if we'll get prod==0 then we'll set prod=1 because 0*any would become zero. 
-  */
+
     int maxProduct(vector<int>& nums){
        int n=nums.size(),prod=1,maxi=INT_MIN;
       for(int i=0;i<n;i++)
@@ -39,5 +39,23 @@ public:
         maxi=max(maxi,prod);
       }
        return maxi;
-    }
+    }*/
+  //TC:=O(N) SC:=>O(1)
+  /*
+  prod1=store maximum prod till current element
+  prod2=store minimum prod till current element
+  result =store max product of all of subarray
+  */
+  int maxProduct(vector<int>&nums)
+  {
+     int prod1 = nums[0],prod2 = nums[0],result = nums[0],n=nums.size();
+     for(int i=1;i<n;i++)
+     {
+        int temp = max({nums[i],prod1*nums[i],prod2*nums[i]});
+        prod2 = min({nums[i],prod1*nums[i],prod2*nums[i]});
+        prod1 = temp; 
+        result = max(result,prod1);
+     }  
+   return result;
+  }
 };
