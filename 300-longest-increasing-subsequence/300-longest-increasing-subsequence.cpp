ec0689c7d8f564c0f88215da94c2f263cc256c42
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-25 12:03:33 +0530

    Time: 1976 ms (5.03%), Space: 38.6 MB (5.01%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+  // initialize dp array with maximum constraints
+    int dp[2501][2501];
+	// ind is current index and prev is last index what we need to compare current indexed element with
+	// prev may be -1 that means its our first element of some temporary sequence
+    int solve(vector<int>& v,int ind,int prev){
+        int n=v.size();
+        if(ind==n) return 0;
+        // we cant store negative index so leave this case
+        if(prev>-1 and dp[ind][prev]!=-1) return dp[ind][prev];
+        int res=0;
+		
+		// if we dont take current element 
+        res = solve(v,ind+1,prev);
+		
+		// if we consider current element
+        if(prev==-1 or v[ind]>v[prev])
+        res = max(res,1+solve(v,ind+1,ind));
+		
+		 // we cant store negative index so leave this case
+        if(prev>-1) dp[ind][prev]=res;
+        return res;
+        
+    }
+    
+    int lengthOfLIS(vector<int>& v) {
+        memset(dp,-1,sizeof(dp));
+        return solve(v,0,-1);
+    }
+  /*
+  //TC:->O(N^2) SC:=>O(N)
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
          vector<int>lis(n,1);
@@ -12,5 +42,5 @@ public:
            }
         }
       return *max_element(lis.begin(),lis.end());
-    }
+    }*/
 };
