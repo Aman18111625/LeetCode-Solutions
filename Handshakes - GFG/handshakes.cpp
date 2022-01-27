978c061f342eff7f06f9f97a61c1df85ac24ac2b
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-27 10:58:33 +0530

    Added solution - LeetHub

---
+++ Handshakes - GFG/handshakes.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int count(int N){
+        // code here
+        if(N%2) return 0;//even number
+        if(N==0) return 1;//base case
+        int res=0;
+        for(int i=0;i<N;i+=2)
+        {
+            res+=(count(i)*count(N-i-2));
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.count(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
