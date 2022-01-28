4d0c6956cd022f12be9704b2dd341b2c4b2723be
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-28 16:54:36 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (34.02%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /*
     vector<int> lexicalOrder(int n) {
         vector<string>s;
         for(int i=1;i<=n;i++) s.push_back(to_string(i));
@@ -7,5 +8,17 @@ public:
         vector<int>res;
         for(int i=0;i<n;i++) res.push_back(stoi(s[i]));
         return res;
+    }*/
+    vector<int>ans;
+    void rec(int n, int s){
+        if(s>n) return;
+        ans.push_back(s);
+        rec(n, s*10);
+        if((s+1)%10) rec(n, s+1);
+    }
+    vector<int> lexicalOrder(int n) {
+        ans.clear();
+        rec(n,1);
+        return ans;
     }
 };
