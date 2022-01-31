795e061246a72221e7d16816b086c3cceb0ef6ba
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-31 12:07:28 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (68.75%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,4 +1,5 @@
 class Solution {
+  /*
   int dp[101][101];
   int helper(vector<vector<int>>&grid,int i,int j)
   {
@@ -14,5 +15,25 @@ public:
        int n=grid.size(),m=grid[0].size();
        memset(dp,-1,sizeof(dp));
       return helper(grid,n-1,m-1);
+    }*/
+  public:
+    int uniquePathsWithObstacles(vector<vector<int>>&grid){
+      int n=grid.size(),m=grid[0].size();
+      int dp[n][m];
+      for(int i=0;i<n;i++)
+      {
+        for(int j=0;j<m;j++)
+        {
+          if(grid[i][j]==1) dp[i][j]=0;
+          else if(i==0 && j==0) dp[i][j]=1;
+          else {
+            int left=0,up=0;
+            if(i>0) up=dp[i-1][j];
+            if(j>0) left=dp[i][j-1];
+            dp[i][j]=left+up;
+          }
+        }
+      }
+      return dp[n-1][m-1];
     }
 };
