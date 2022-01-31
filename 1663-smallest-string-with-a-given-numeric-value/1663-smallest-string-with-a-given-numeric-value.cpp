ea4228e0784f0720d67a92df4c4ebca01c1cb033
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-31 14:16:54 +0530

    Time: 38 ms (82.98%), Space: 21 MB (99.09%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+  /*
     string getSmallestString(int n, int k) {
         if(n==k) return string(n,'a');
         string arr=string(n,'a');
         //now traverse from at the end
         for(int i=n-1;i>=0;i--)
         {
-          k-=i;
+           k-=i;
           if(k>=0)
           {
             if(k>=26)
@@ -23,5 +24,22 @@ public:
           k+=i;
         }
       return arr;
+    }*/
+    string getSmallestString(int n, int k) {
+        string res(n, 'a');
+//since we want the lexicographically smallest string,therefore put n characters = a
+// now the k should reduce by n since we placed n a's
+         k-=n;
+        int ptr = n-1;
+        // ptr will point to end of string so that from the end we can start adding 
+        // z's or whatever character is required
+        // loop till k is > 0, because k is the sum which we have to achiveve. We'll go on reducing it.
+        while (k) {
+            // res[ptr] was 'a', now if we add 25 to 'a' it becomes 'z'
+            // but let's say k initially was 27, and currently k = 5, means current sum = 22. so required sum = 5, so there's no point of adding 25. Therefore we take min of k and 25
+            res[ptr--] += min(k, 25);
+            k -= min(k, 25);
+        }
+        return res;
     }
 };
