05183f5e7fef2c1f54f9b1cce458b8736d895739
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-31 11:29:19 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.68%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,4 +1,5 @@
 class Solution {
+  /*Recursive and memoization: TC:=>O(M*N) SC:=>O(M*N)
   int dp[101][101];
   int helper(int i,int j,int &m,int &n)
   {
@@ -13,5 +14,25 @@ public:
     int uniquePaths(int m, int n) {
         memset(dp,-1,sizeof(dp));
         return helper(0,0,m,n);
+    }*/
+  public:
+  //Bottom-up DP
+  //TC:=>O(M*N) SC:=>(M*N)
+  int uniquePaths(int m, int n) {
+        int dp[m][n];
+        //from top to down
+        for(int i = 0; i < m; ++i)
+            dp[i][0] = 1;
+        //from left to right
+        for(int j = 1; j < n; ++j)
+            dp[0][j] = 1;
+        
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
     }
 };
