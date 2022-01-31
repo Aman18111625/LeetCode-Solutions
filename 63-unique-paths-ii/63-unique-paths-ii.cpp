c3d1078c1c03fc7e08c7c24902571a854a993ef3
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-01-31 12:21:27 +0530

    Time: 7 ms (25.95%), Space: 7.6 MB (68.75%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
   /*
+  //Recursive+Memoization
+  //TC:=>O(N*M) SC:=>O(N*M)
   int dp[101][101];
   int helper(vector<vector<int>>&grid,int i,int j)
   {
@@ -17,6 +19,8 @@ public:
       return helper(grid,n-1,m-1);
     }*/
   public:
+  /*Bottom -Up Approach 
+  //TC:=>O(N*M) SC:=>O(N*M)
     int uniquePathsWithObstacles(vector<vector<int>>&grid){
       int n=grid.size(),m=grid[0].size();
       int dp[n][m];
@@ -35,5 +39,29 @@ public:
         }
       }
       return dp[n-1][m-1];
-    }
+    }*/
+  
+  //Space-Optimization
+  //TC:=>O(N*M)
+  //SC:=>O(N)
+  /*
+If we closely look the relation,
+dp[i][j] = dp[i-1][j] + dp[i][j-1])
+We see that we only need the previous row and column,in order to calculate dp[i][j]. Therefore we can space optimize it.
+Initially, we can take a dummy row ( say prev) and initialize it as 0.
+Now the current row only needs the previous row value and the current row’s value in order to calculate dp[i][j].
+*/
+   int uniquePathsWithObstacles(vector<vector<int>>&grid)
+   {
+        int row = grid.size(), col = grid[0].size();
+        vector<int> res(col, 0);
+        res[0] = 1;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (grid[i][j]) res[j] = 0;//if grid[i][j] is obstacle then simply store zero else check for previous row
+                else if (j > 0) res[j] += res[j - 1];
+            }
+        }
+        return res[col - 1];
+   }
 };
