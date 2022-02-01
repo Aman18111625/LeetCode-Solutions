ef149abb8924db3d8fe72798ee6249a80b63ae7c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-01 11:32:12 +0530

    Time: 116 ms (80.08%), Space: 28.3 MB (77.25%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  int dp[2001][2002];
+  bool helper(vector<int>&stones,int start=0,int jump=0)
+  {
+    if(start==stones.size()-1) return true;//we reach the (n-1)th position
+    if(dp[start][jump]!=-1) return dp[start][jump];
+    bool flag=false;
+    int curr=stones[start];
+    for(int i=start+1;i<stones.size() && !flag;i++)
+    {
+      int need=stones[i]-curr;
+      if(need-jump>1) break;
+      else if(need-jump>=-1 && need-jump<=1)
+      {
+        flag=helper(stones,i,need);
+      }
+    }
+    return dp[start][jump]=flag;
+  }
+public:
+    bool canCross(vector<int>& stones) {
+        int n=stones.size();
+      if(n==2) return stones[1]<=1;
+      memset(dp,-1,sizeof(dp));
+      return helper(stones,0,0);
+    }
+};
+
+ 
+    
