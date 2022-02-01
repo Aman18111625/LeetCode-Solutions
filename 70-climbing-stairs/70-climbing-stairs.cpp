4e0ce4d02ccbcadd2fb517fe02698eec13aed997
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-01 09:51:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.47%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -5,11 +5,28 @@ public:
         if(n<2) return 1;
         return climbStairs(n-1)+climbStairs(n-2);
     }*/
+    int dp[46];
+   int helper(int n)
+   {
+     if(n<2) return dp[n]=1;
+     if(dp[n]!=-1) return dp[n];
+     int op1=helper(n-1);
+     int op2=helper(n-2);
+     dp[n]=op1+op2;
+     return dp[n];
+   }
+   int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+     return helper(n);
+    }
+    
+  /*
+  //Bottom-up approach :=>TC =(N) SC=O(N)
     int climbStairs(int n){
        int dp[n+1];
       dp[0]=0,dp[1]=1;
       if(n>=2) dp[2]=2;
       for(int i=3;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
       return dp[n];
-    }
+    }*/
 };
