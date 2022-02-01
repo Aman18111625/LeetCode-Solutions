2c0e981d07218e163b8847a0cc492140d00da8d2
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-01 09:37:15 +0530

    Time: 10 ms (34.55%), Space: 8.3 MB (37.78%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /* TC:=>O(N^2) SC:=>O(1)
     int maximumDifference(vector<int>& nums) {
         int n=nums.size();
         int maxi=-1;
@@ -11,5 +12,15 @@ public:
           }
         }
       return maxi;
-    }
+    }*/
+  int maximumDifference(vector<int>& nums){
+    int curr=INT_MAX,maxi=-1;
+     for(auto &it: nums)
+     {
+       curr=min(curr,it);
+       maxi=max(maxi,it-curr);
+     }
+      if(maxi==0) return -1;
+      return maxi;
+  }
 };
