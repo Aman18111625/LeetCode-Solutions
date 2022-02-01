1d819b951dfc3cf3c6c4d978e868bac8cbaf09d9
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-01 22:17:01 +0530

    Time: 29 ms (5.36%), Space: 9.4 MB (5.20%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+       // if(s.length()!=t.length()) return false;
+         unordered_map<char,vector<int>>s1;
+         unordered_map<char,vector<int>>s2;
+        for(int i=0;i<s.length();i++)
+        {
+            s1[s[i]].push_back(i);
+            s2[t[i]].push_back(i);
+            if(s1[s[i]]!=s2[t[i]]) return false;
+        }
+        return true; 
+    }
+};
