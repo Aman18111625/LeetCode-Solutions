05ec96bcb90641e4392366cf49b977930c523871
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-02 21:53:29 +0530

    Time: 8 ms (96.54%), Space: 15.9 MB (17.62%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-      if(target.size()!=arr.size()) return false;
+     /* if(target.size()!=arr.size()) return false;
         sort(target.begin(),target.end());
         sort(arr.begin(),arr.end());
-      return target==arr;
+      return target==arr;*/
+         map<int,int>mp;
+        for(int i = 0 ; i < arr.size() ; i++){
+            mp[arr[i]]++;
+            mp[target[i]]--;
+        }
+        for(auto it : mp) if(it.second) return false;
+        return true;
     }
 };
