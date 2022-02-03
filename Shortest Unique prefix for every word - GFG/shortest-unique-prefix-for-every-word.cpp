c71704fd7df4f4bc5f94e11127b40901bd7c8243
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-04 00:44:55 +0530

    Added solution - LeetHub

---
+++ Shortest Unique prefix for every word - GFG/shortest-unique-prefix-for-every-word.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    
+    struct Node{
+      int cnt{0};
+      Node* children[26]={0};
+    };
+    
+    void insert(Node* root,string& word){
+        for(auto ch: word)
+        {
+            int c=ch-'a';
+            if(!root->children[c]) root->children[c]= new Node;
+            root->cnt++;
+            root=root->children[c];
+        }
+    }
+    
+    string getPrefix(Node* root,string& word)
+    {
+        string prefix="";
+        for(auto &ch: word)
+        {
+            int c=ch-'a';
+            if(root->cnt==1) break;
+            prefix+=ch;
+            root=root->children[c];
+        }
+        return prefix;
+    }
+    
+    vector<string> findPrefixes(string arr[], int n)
+    {
+        //code here
+        Node* trie=new Node();
+        for(int i=0;i<n;i++)
+          insert(trie,arr[i]);
+          
+          vector<string>ans;
+          for(int i=0;i<n;i++)
+           ans.push_back(getPrefix(trie,arr[i]));
+          
+          return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+            
+        Solution ob;
+        vector<string> ans = ob.findPrefixes(arr, n);
+        
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] <<" ";
+
+
+        cout << "\n";
+
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
