da69005359f00c317138d13b99e1c82f35291d4b
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-04 11:25:56 +0530

    Time: 7 ms (90.94%), Space: 9.9 MB (58.64%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,9 +1,12 @@
 class Solution {
+  /*Recursive + Memoization 
+  Time-Complexity:=>O(N*M)
+  Space-Complexity:=>O(N*M)+O(N)(space-stack)
   int dp[201][201];
   int helper(vector<vector<int>>&grid,int i,int j,int& n,int &m){
             //out of matrix
            if(i>=n || j>=m) return INT_MAX;
-            //base case -> we reacj extreme bottom right corner
+            //base case -> we reach at extreme bottom right corner
            if(i==n-1 && j==m-1) return dp[i][j]=grid[i][j];
            if(dp[i][j]!=-1) return dp[i][j];
            //now we have two option we can either go to down or go to right
@@ -16,6 +19,39 @@ public:
          int n=grid.size(),m=grid[0].size();
          memset(dp,-1,sizeof(dp));
          return helper(grid,0,0,n,m);
-    }
+    }*/
+  public:
+  //Bottom-up Solution 
+  //Time-Complexity:=>O(N*M)
+  //Space-Complexity:=>O(N*M)
+  int minPathSum(vector<vector<int>> &grid) {
+    // Write your code here.
+        int n=grid.size();
+        int m=grid[0].size();
+        int dp[n][m];
+        for(int i=n-1;i>=0;i--)
+        {
+         for(int j=m-1;j>=0;j--)
+         {
+              if(i==n-1 and j==m-1) //last index 
+              {
+               dp[i][j]=grid[i][j];   
+              }
+             else if(i==n-1) //last row
+             {
+                 dp[i][j]=dp[i][j+1]+grid[i][j];
+             }
+             else if(j==m-1) //last column
+             {
+                 dp[i][j]=dp[i+1][j]+grid[i][j];
+             }
+             else
+             {
+                 dp[i][j]=min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+             }
+         }
+        }
+        return dp[0][0];
+}
 };
 
