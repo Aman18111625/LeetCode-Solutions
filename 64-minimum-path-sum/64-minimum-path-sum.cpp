1d342cb3e8daf2b8ce44ba9bd5b54291657b8625
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-04 11:34:28 +0530

    Time: 10 ms (62.66%), Space: 9.5 MB (97.70%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -21,6 +21,7 @@ public:
          return helper(grid,0,0,n,m);
     }*/
   public:
+  /*
   //Bottom-up Solution 
   //Time-Complexity:=>O(N*M)
   //Space-Complexity:=>O(N*M)
@@ -52,6 +53,26 @@ public:
          }
         }
         return dp[0][0];
-}
+}*/
+  //Space-Optimization
+  //TC:=>O(N*M)
+  //SC:=>O(1)
+      int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        
+         for(int i=1;i<n;i++) 
+           grid[i][0]+=grid[i-1][0];
+        
+        for(int j=1;j<m;j++)
+         grid[0][j]+=grid[0][j-1];
+        
+     for(int i=1;i<n;i++)
+         for(int j=1;j<m;j++)
+             grid[i][j] = min(grid[i-1][j],grid[i][j-1]) + grid[i][j];
+        
+        return grid[n-1][m-1];
+    }
 };
 
