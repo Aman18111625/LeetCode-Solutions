274627a25d1389eb52b5eb896fc815287db97c5d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-05 10:41:21 +0530

    Time: 20 ms (91.54%), Space: 13.8 MB (29.97%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -9,7 +9,26 @@
  * };
  */
 class Solution {
+   struct node{
+       ListNode *curr;
+        int idx;
+        node(ListNode *a,int b)
+        {
+           curr=a;
+            idx=b;
+        }
+    };
+    struct cmp{
+      bool operator()(node a,node b)
+      {
+        return a.curr->val > b.curr->val; 
+      }
+    };
 public:
+  /*
+  //Time-Complextity:=>O(N*k)+O(Nklogn(NK));
+  //Space-Complexity:=>O(N*k)
+  //store all the value in the vector then sort it and create a new List
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         vector<int>ans;
          int n=lists.size();
@@ -33,5 +52,41 @@ public:
          temp=temp->next;
        }
       return dummy->next;
+    }*/
+  //Approach-2 
+  //Time-Complexity:O(N)
+  //Space-Complexity:O(NK)
+  //initally we have pushed all the head of the k lists into pq then take top of pq and move to the next of top && if it's not null then push into pq and again do the same process again and again untill pq is not empty
+  ListNode* mergeKLists(vector<ListNode*>& lists){
+    int n=lists.size();
+    if(n==0) return NULL;
+    if(n==1) return lists[0];
+    ListNode* head=NULL,*tail=NULL;
+    priority_queue<node,vector<node>,cmp>heap;
+    vector<ListNode*>ptr(n);
+    for(int i=0;i<n;i++)
+    {
+      ptr[i]=lists[i];
+      if(ptr[i]) heap.push(node(ptr[i],i));
     }
+    if(heap.empty()) return NULL;
+    head=tail=heap.top().curr;
+    int idx=heap.top().idx;
+    heap.pop();
+    ptr[idx]=ptr[idx]->next;
+    if(ptr[idx]) heap.push(node(ptr[idx],idx));
+    while(!heap.empty())
+    {
+      ListNode* temp=heap.top().curr;
+      idx=heap.top().idx;
+      heap.pop();
+      tail->next=temp;
+      tail=tail->next;
+      ptr[idx]=ptr[idx]->next;
+      if(ptr[idx]) heap.push(node(ptr[idx],idx));
+    }
+    return head;
+  }
 };
+
+
