f551b46dd128661a5b3889001111e87fcb30c422
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-06 17:08:31 +0530

    Time: 6 ms (19.65%), Space: 7.6 MB (16.24%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,9 +10,10 @@
  */
 class Solution {
 public:
+  /*
     ListNode* reverseBetween(ListNode* head, int i, int j) {
         //base case
-         if(!head || i==j) return head;
+        if(!head || i==j) return head;
         ListNode* prev=NULL,*curr=head;
         for(int l=0;curr && l<i-1;l++)
         {
@@ -35,6 +36,33 @@ public:
         else head=prev;
         newend->next=curr;//curr will be updated toh the r+1th element so joining newend to that
       return head;
+    }*/
+   ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int i;
+        ListNode* curr=head;
+        ListNode* newnode=new ListNode;
+        ListNode* t=newnode;
+        vector<int>v;
+        
+        while(curr)
+        {
+            v.push_back(curr->val);
+            curr=curr->next;
+        }
+        reverse(v.begin()+left-1,v.begin()+right);
+        for(i=0;i<v.size();i++)
+        {
+            ListNode* temp=new ListNode(v[i]);
+            if(!newnode)
+                newnode=temp;
+            else
+            {
+                newnode->next=temp;
+                newnode=newnode->next;
+            }
+        }
+        
+        return t->next;
     }
 };
  
