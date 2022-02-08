0d9c33d6a8f4e709a737657fb7e41d67112035a5
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-08 11:17:16 +0530

    Time: 4 ms (43.34%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,4 +1,5 @@
 class Solution {
+  /*
   int getDigit(int n)
   {
     int cnt=0;
@@ -20,5 +21,20 @@ public:
           num/=10;
         }
       return addDigits(sum);
-    }
+    }*/
+  public:
+  int addDigits(int num)
+  {
+        int sum=0;
+        while(num)
+        {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else
+            return addDigits(sum);
+  }
+ 
 };
