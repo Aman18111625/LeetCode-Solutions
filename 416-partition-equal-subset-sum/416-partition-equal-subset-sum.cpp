1f512f33887fd56253bbd1e454d77e54f087b0ca
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-09 10:46:10 +0530

    Time: 85 ms (74.79%), Space: 76 MB (12.78%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,8 +3,9 @@ class Solution {
      if(sum==0) return true;//it means sum is present
      if(n<=0) return false;//we exhausted the given vector
      //if nums[i]>sum then we'll not include it otherwise for every nums[i] we have two options rather we can include it or not
-     if(dp[n][sum]!=-1) return dp[n][sum];
-     if(nums[n-1]>sum) return dp[n][sum]=helper(nums,n-1,sum,dp);
+     if(dp[n][sum]!=-1) return dp[n][sum];//already calculated
+     if(nums[n-1]>sum) return dp[n][sum]=helper(nums,n-1,sum,dp);//let's say required sum is 2 and nums[i] >2 then it'll never give us our answer if we include it
+     //now for every nums[i] we have two choise either we can take it or not take it 
     return dp[n][sum]=helper(nums,n-1,sum-nums[n-1],dp) || helper(nums,n-1,sum,dp);
    }
 public:
@@ -12,7 +13,7 @@ public:
         int sum=0,n=nums.size();
         for(auto &num : nums) sum+=num;
         if(sum%2) return false;
-        sum/=2;
+        sum>>=1;
         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
         return helper(nums,n,sum,dp);
     }
