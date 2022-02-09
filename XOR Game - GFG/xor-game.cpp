d5d8f01a69f4b52f6fc0f7af6e27417ae062f45f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-10 00:19:06 +0530

    Added solution - LeetHub

---
+++ XOR Game - GFG/xor-game.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int xorCal(int k){
+        // code here
+        if(k==1) return 2;
+        int n=k/2;
+        int x=n^n+1;
+        if(x==k) return n;
+         return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        cout<<ob.xorCal(k)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
