828958e8f6bcbcb7650fbe0fab3697d0e6ab347b
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-09 10:33:26 +0530

    Time: 26 ms (51.47%), Space: 13.3 MB (58.82%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /*
   //Approach-1 Using Two Pointer Method
   //Time-Complexity=>O(NLogN)(sorting)+O(N)+O(N)
   //Space-Complexity:=>O(1)
@@ -21,11 +22,36 @@ public:
            high++;
          }else{
            low++;
-           if(low==high) high++;
+           //if(low==high) high++;
          }
        }
     return cnt;
+    }*/
+  //Approach-2 Using Hashmap
+  //TC:=>O(N)
+  //SC:=>O(N)
+  int findPairs(vector<int>& nums, int k){
+    unordered_map<int,int>mpp;
+    for(auto &it : nums) mpp[it]++;
+    int cnt=0;
+    for(auto &it : mpp)
+    {
+      if(k==0)//in this same number is giving us pair like 3-3==0 or 1-1==0
+      {
+        if(it.second>1) 
+        {
+          cnt++;
+        }
+      }else 
+        {
+          if(mpp.find((it.first-k))!=mpp.end())
+          {
+            cnt++;
+          }
+        }
     }
+      return cnt;
+  }
 };
 
  
