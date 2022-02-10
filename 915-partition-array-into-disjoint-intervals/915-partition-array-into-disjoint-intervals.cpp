5f3bd11a4c81afd4fed992ab6304bb28505f0043
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-10 12:12:29 +0530

    Time: 160 ms (73.78%), Space: 95.1 MB (56.29%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /*
   #define pi pair<int,int>
   //Approach-1 Using Max & Min Heap
   //TC;=>O(N)
@@ -25,5 +26,26 @@ public:
                 return i+1;   
         }
         return -1; // if no such answer possible
+    }*/
+  //Approach-2 Using Left and right array
+   int partitionDisjoint(vector<int>& nums) {
+        int n=nums.size();
+        int left[n],right[n];
+        left[0]=nums[0];
+        right[n-1]=nums[n-1];
+        //left->max and right->min
+        for(int i=1;i<n;i++)
+        {
+             left[i]=max(left[i-1],nums[i]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+          right[i]=min(nums[i],right[i+1]);  
+        }
+        for(int i=0;i<n;i++)
+        {
+         if(left[i]<=right[i+1]) return i+1;  
+        }
+        return -1;
     }
 };
