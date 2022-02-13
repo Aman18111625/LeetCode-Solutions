59ef04041bd5784dfe1740008a8546b458bbd418
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-13 15:44:44 +0530

    Added solution - LeetHub

--- Maximum number of events that can be attended - GFG/maximum-number-of-events-that-can-be-attended.cpp
+++ Maximum number of events that can be attended - GFG/maximum-number-of-events-that-can-be-attended.cpp
@@ -10,28 +10,35 @@ class Solution {
   public:
     int maxEvents(int start[], int end[], int N) {
         // code here
-        priority_queue <int, vector<int>, greater<int>> pq;
         vector<pair<int,int>> A(N);
         for(int i=0; i<N; i++)
         {
             A[i].first = start[i];
             A[i].second = end[i];
         }
-        sort(A.begin(),A.end());
-        
-        int i = 0, res = 0, d = 0;
-        while (!pq.empty() || i < N) {
-            if (pq.empty())
-                d = A[i].first;
-            while (i < N && A[i].first <= d)
-                pq.push(A[i++].second);
-            pq.pop();
-            ++res, ++d;
-            while (!pq.empty() && pq.top() < d)
-                pq.pop();
-
-        }
-        return res;
+        sort(A.begin(),A.end());//sort according to end time
+        unordered_set<int>st;
+        //A[i].second->end day ,A[i].first->start day
+        //we'll try to attend all the events at late as possible 
+         for(int i=N-1;i>=0;i--)
+         {
+             //i can attend at last day
+             if(st.count(A[i].second)==0)
+             {
+                 st.insert(A[i].second);
+             }else{//search from end to start in which day it's possible to attend that events
+                 int e=A[i].second;
+                 while(e>=A[i].first)
+                 {
+                    if(st.count(e)==0){
+                      st.insert(e);
+                      break;
+                    }
+                     e--;
+                 }
+             }
+         }
+        return st.size();
     }
 };
 
