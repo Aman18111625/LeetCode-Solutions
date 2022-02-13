1e3ca95c78d2232ff214147c2f299b4246bc19f2
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-13 22:42:01 +0530

    Time: 8 ms (75.71%), Space: 10 MB (53.25%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    int deleteAndEarn(vector<int>& nums) {
+  /*Tabulation:=>
+  //Time-Complexity:=>O(N)
+  //Space-Complexity:=>O(N)
+    int deleteAndEarn(vector<int>&nums) {
       int n = 10001;
 	 //take the total sum by each number
      vector<int> sum(n, 0);
@@ -16,5 +19,22 @@ public:
         dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
     
     return dp[n-1];
+    }*/
+    int result(vector<int>&arr,int i,vector<int>&dp){
+        if(i==0) return arr[i];
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=0,nottake=0;
+        nottake = result(arr,i-1,dp);
+        take = result(arr,i-2,dp)+arr[i];
+        return dp[i]=max(take,nottake);
     }
+     int deleteAndEarn(vector<int>&nums){
+        int maxi=INT_MIN;
+        for(auto &it : nums) maxi=max(maxi,it);
+        vector<int>arr(maxi+1,0),dp(maxi+1,-1);
+        for(int i:nums)
+            arr[i]+=i;
+       return result(arr,maxi,dp);
+     }
 };
