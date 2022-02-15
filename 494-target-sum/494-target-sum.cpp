7a0d3529041960647c432f744ae04cb1a3b55d4c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-15 10:32:19 +0530

    Time: 15 ms (73.02%), Space: 9 MB (78.10%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -30,6 +30,8 @@ public:
         helper(nums,0,n,"-",target,0);
       return cnt/2;
     }*/
+  /*
+  //Recursive + Memoization
      int dp[21][2001];
     int helper(vector<int>&nums,int index,int n,int target,int sum)
     {
@@ -45,5 +47,42 @@ public:
         int n=nums.size();
         memset(dp,-1,sizeof(dp));
       return  helper(nums,0,n,target,0);
+    }*/
+  //This is similar to count number of way to divide to array in two subset such that sum equal to target
+    int countSubsets(vector<int>& nums, int n, int sum)
+    {
+        int dp[n + 1][sum+ 1];
+      //intialization
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= sum; j++)
+            {
+                if(i == 0) dp[i][j] = 0;
+                if(j == 0)  dp[i][j] = 1;
+            }
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 0; j <= sum; j++)
+            {
+                if(nums[i - 1] <= j)
+                 dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                else
+                  dp[i][j] = dp[i - 1][j];
+            }
+        }
+        
+        return dp[n][sum];  
     }
+    public:
+    int findTargetSumWays(vector<int>& nums, int target)
+     {
+         target = abs(target);
+         int sum = 0;
+         for(auto& num : nums) sum += num;
+          if(sum < target or ((sum + target)&1))  return 0;
+          int M = (sum + target)/2;
+         return countSubsets(nums, nums.size(), M);
+     }
 };
