0cafbe5e77a4dce6ae8205b8cdbfb0939213589f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-17 12:34:46 +0530

    Time: 122 ms (7.07%), Space: 9 MB (62.09%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+  /*Brute-Force:=>Using Min-Heap
+  TC:=>O(N)
+  SC:=>O(N)
     int largestSumAfterKNegations(vector<int>& nums, int k) {
        priority_queue<int,vector<int>,greater<int>>pq;//min-heap
        for(auto &it : nums)
@@ -15,5 +18,32 @@ public:
       int sum=0;
       while(!pq.empty()) sum+=pq.top(),pq.pop();
       return sum;
-    }
+    }*/
+  int largestSumAfterKNegations(vector<int>&nums,int k)
+  {
+          int i=0;
+        while(k)
+        {   
+            sort(nums.begin(),nums.end());
+            if(nums[i]<0)
+            {
+                nums[i]= -nums[i];
+                k--;
+            }
+            else if(nums[i]==0)
+            {
+                nums[i]=nums[i];
+                k=k-k;
+            }
+            else
+            {
+                nums[i]=-nums[i];
+                k--;
+            }
+        }
+        int sum=0;
+        for(auto i:nums)
+            sum+=i;
+        return sum;
+  }
 };
