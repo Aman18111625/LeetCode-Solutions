3b6cd9bfe53d4d4520f5213d0426c868daef48e6
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-17 10:54:58 +0530

    Time: 55 ms (27.58%), Space: 18.8 MB (19.14%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,12 +1,20 @@
 class Solution {
+  //Recursive+Memoization
+  //Time-Complexity:=>O(N*amount)
+  //Space-Complexity:=>O(N*amount)+O(N)
 int helper(vector<int>&coins,int index,int n,int amount,vector<vector<int>>&dp)
     {
-        if(amount==0) return 1;
-        if(amount<0 or index>=n) return 0;
+        if(amount==0) return 1;//we got desired amount
+        if(amount<0 or index>=n) return 0;//out of bound
         if(dp[index][amount]!=-1) return dp[index][amount];
-        int left=helper(coins,index,n,amount-coins[index],dp);
-        int right=helper(coins,index+1,n,amount,dp);
-        return dp[index][amount]=left+right;
+  //for every number we have two choices ->we can choose it and move to the same index or ignore & move to next index
+        int take=0,notTake=0;
+        if(coins[index]<=amount)
+        {
+          take=helper(coins,index,n,amount-coins[index],dp);
+        }
+         notTake=helper(coins,index+1,n,amount,dp);
+        return dp[index][amount]=take+notTake;
     }
 public:
     int change(int amount, vector<int>& coins) {
