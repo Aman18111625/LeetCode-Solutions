d390bc75480988a93f090f3ffe17c6e2f601f7f4
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-17 21:55:12 +0530

    Time: 31 ms (33.84%), Space: 24.5 MB (73.81%) - LeetHub

--- 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
@@ -10,11 +10,9 @@
  * };
  */
 class Solution {
-     unordered_map<int,int>mpp;
-     int max_freq=0;
-     int helper(TreeNode* root){
+     int helper(TreeNode* root,unordered_map<int,int>&mpp,int &max_freq){
      if(!root) return 0;
-     int sum=root->val+helper(root->left)+helper(root->right);
+     int sum=root->val+helper(root->left,mpp,max_freq)+helper(root->right,mpp,max_freq);
      mpp[sum]++;
      max_freq=max(max_freq,mpp[sum]);
      return sum;
@@ -23,7 +21,9 @@ public:
     vector<int> findFrequentTreeSum(TreeNode* root) {
          vector<int>ans;
          if(!root) return ans;
-         helper(root);
+         unordered_map<int,int>mpp;
+         int max_freq=0;
+         helper(root,mpp,max_freq);
          for(auto &it : mpp)
          {
            if(it.second==max_freq) ans.push_back(it.first);
