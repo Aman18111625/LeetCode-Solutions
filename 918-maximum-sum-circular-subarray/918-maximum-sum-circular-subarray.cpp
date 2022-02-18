5d913db889e141e057918e8dff77aa308529fa53
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-18 10:56:52 +0530

    Time: 122 ms (21.91%), Space: 39.8 MB (72.48%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+  /*
+Here solution has 2 cases:
+1. if max_subarray lies within the indexes in a straight path.
+2. if the max subarray is split, then it means that min-subbaray is present in a staright path, so answer might be total sum-min_sum;
+However one edge case needs to be handeled when all values are negative min_sum==total_sum, in that case max_array in staight path iour answer.
+  //Best Optimal Approach
+  //Time-Complexity:=>O(N)
+  //Space-Complexity:=>O(1)
     int maxSubarraySumCircular(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
@@ -21,5 +29,30 @@ public:
          return maxSum;
        }
       return max(maxSum,sum-minSum);
+    }*/
+  
+int kadane(vector<int>&nums)
+{
+    int sum = 0;
+    int maxSum = INT_MIN;
+    for(int i=0; i<nums.size(); i++){
+        sum += nums[i];
+        sum = max(sum, nums[i]);
+        maxSum = max(maxSum, sum);
     }
+    return maxSum;
+}
+  int maxSubarraySumCircular(vector<int>&nums) {
+    int maxSum1 = kadane(nums);
+    int sum = 0; 
+    for(int i=0; i<nums.size(); i++){
+        sum += nums[i];
+        nums[i] = nums[i]*-1;    // nums[i]*-1 is -nums[i]
+    }
+    int maxSum2 = kadane(nums);
+    if(maxSum1<0)//all elements are -ve
+         return maxSum1;
+    
+    return max(maxSum1, sum + maxSum2);   // sum+maxSum2 == sum - (-maxSum2)
+}
 };
