f72f4e24413e48f61085ea2cf6b770f6dd80621f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-19 16:45:59 +0530

    Time: 294 ms (56.95%), Space: 71.2 MB (83.44%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+  //Asked in Amazon and Samsung
     int minimumDeviation(vector<int>&nums) {
         int n=nums.size();
-        int mini=INT_MAX;
+        int mini=INT_MAX,maxi=INT_MIN;
         priority_queue<int>pq;//maxHeap
         for(int i=0;i<n;i++){
             if(nums[i]&1) nums[i]*=2;//if odd then we'll multiply by 2 
             mini=min(mini,nums[i]);//take care of minimum after performing operation
+           maxi=max(maxi,nums[i]);//take care of maximum
             pq.push(nums[i]);
         } 
-        int ans=INT_MAX;
-        while(!pq.empty() && pq.top()%2==0){
-           int maxi=pq.top(); pq.pop();
+        int ans=maxi-mini;
+        while(pq.top()%2==0){
+            int maxi=pq.top(); 
+             pq.pop();
             ans=min(ans,maxi-mini);
-            mini=min(mini,maxi/2);
-            pq.push(maxi/2);
+            maxi>>=1;
+            mini=min(mini,maxi);
+            pq.push(maxi);
         }
       return min(ans,(pq.top()-mini));
     }
