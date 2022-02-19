0b71bfa4ac9e939760a9eff7a725af1bef84ded7
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-19 17:12:19 +0530

    Time: 731 ms (15.89%), Space: 121.3 MB (10.60%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-  //Asked in Amazon and Samsung
+  /*Using Priority_Queue
+  //Previosuly Asked in Amazon and Samsung
     int minimumDeviation(vector<int>&nums) {
         int n=nums.size();
         int mini=INT_MAX,maxi=INT_MIN;
-        priority_queue<int>pq;//maxHeap
+        priority_queue<int>pq;//maxHeap->using this because it'll give us everytime max element in O(logn) time 
+      //we can perform 2 operations so *2->increasing and /2->decreasing so what i'll do here *2 to all odd so at the i have to perform only one operations
         for(int i=0;i<n;i++){
             if(nums[i]&1) nums[i]*=2;//if odd then we'll multiply by 2 
             mini=min(mini,nums[i]);//take care of minimum after performing operation
@@ -21,5 +23,23 @@ public:
             pq.push(maxi);
         }
       return min(ans,(pq.top()-mini));
+    }*/
+  //Using Set->instead of using priority_queue we can use set too.
+     int minimumDeviation(vector<int>& nums) {
+        set<int>st;
+        for(auto &it : nums) 
+        {
+          if(it&1) st.insert(it*2);
+          else st.insert(it);
+        }
+        int diff = *st.rbegin() - *st.begin();//*st.rbegin()->max element & *st.begin()->min element
+        while(*st.rbegin() % 2 == 0)
+        {
+            int x = *st.rbegin();
+            st.erase(x);
+            st.insert(x/2);
+            diff = min(diff, *st.rbegin() - *st.begin());
+        }
+        return diff;
     }
 };
