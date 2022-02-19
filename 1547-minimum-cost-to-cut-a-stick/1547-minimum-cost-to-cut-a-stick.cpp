854613a2808dc77b09505738f4750415e618bc90
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-19 11:59:35 +0530

    Time: 80 ms (54.30%), Space: 8.3 MB (91.83%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    int dp[102][102];
-    int helper(int start,int end,vector<int>&cuts,int cut_start,int cut_end)
-    {
-        if(cut_start>cut_end) return 0;
-        if(dp[cut_start][cut_end]!=-1) return dp[cut_start][cut_end];
-            int minimum = INT_MAX;
-        for(int i=cut_start; i<=cut_end; i++)
-            minimum = min(minimum, (end-start)+helper(start, cuts[i], cuts, cut_start, i-1)+helper(cuts[i], end, cuts, i+1, cut_end));
-        return dp[cut_start][cut_end]=minimum;
+  int dp[101][101];
+    int solve(vector<int>& cuts, int l, int r, int ci, int cj){
+        if(ci>cj) return 0;
+        if(dp[ci][cj]!=-1) return dp[ci][cj];
+        int cost = 1e7;
+        for(int k=ci; k<=cj; ++k){
+            cost = min(cost, solve(cuts,l,cuts[k],ci,k-1)+solve(cuts,cuts[k],r,k+1,cj));
+        }
+        return dp[ci][cj] = cost+r-l;
     }
 public:
     int minCost(int n, vector<int>& cuts) {
         memset(dp,-1,sizeof(dp));
-        sort(cuts.begin(),cuts.end());
-       return helper(0,n,cuts,0,cuts.size()-1);
+        sort(cuts.begin(),cuts.end());//all the position will be in sorted order
+       return solve(cuts,0,n,0,cuts.size()-1);
     }
 };
