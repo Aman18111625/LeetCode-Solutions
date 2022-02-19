b9da49d2ba82e79c364e9c61b46c842f16a5784c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-19 22:38:48 +0530

    Time: 27 ms (34.57%), Space: 24 MB (8.13%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -10,6 +10,7 @@
  * };
  */
 class Solution {
+  /*
   TreeNode *in(TreeNode *root, int low, int high){
         if(!root) return NULL;        
 //if the current root satisfies the condition then call for the left and the right subtree
@@ -28,5 +29,18 @@ public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if(!root) return root;
         return in(root,low,high);
-    }
+    }*/
+  public:
+  TreeNode* trimBST(TreeNode* root, int low, int high) {
+    if(!root) return NULL;
+    if(root->val < low)
+        return trimBST(root->right, low, high);
+    if(root->val > high)
+        return trimBST(root->left, low, high);
+  
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    
+    return root;
+}
 };
