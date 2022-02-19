f2e8cff12576568957aa5eaf76a4707058d77cf1
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-19 12:02:53 +0530

    Time: 99 ms (40.51%), Space: 10.4 MB (30.57%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,4 +1,5 @@
 class Solution {
+  /*Recursive+Memoization
   int dp[101][101];
     int solve(vector<int>& cuts, int l, int r, int ci, int cj){
         if(ci>cj) return 0;
@@ -14,5 +15,29 @@ public:
         memset(dp,-1,sizeof(dp));
         sort(cuts.begin(),cuts.end());//all the position will be in sorted order
        return solve(cuts,0,n,0,cuts.size()-1);
+    }*/
+  public:
+   int minCost(int n, vector<int>& arr) {
+        vector<int>nums = {0};
+        int size=arr.size();
+        for(int i = 0; i<size;++i) nums.push_back(arr[i]);
+        nums.push_back(n); 
+        sort(nums.begin(),nums.end());
+        int nsize=nums.size();
+        vector<vector<int>>dp(nsize,vector<int>(nsize,INT_MAX));
+        for(int gap = 1; gap<nsize;++gap){
+            for(int i=0,j=gap; j<nsize;++j,++i){
+                if(gap==1) dp[i][j] = 0;
+                else{
+                    int cost = INT_MAX;
+                    for(int k = i+1; k<j;++k){
+                        cost = min(cost,dp[i][k]+dp[k][j]);
+                    }
+                    cost+=nums[j]-nums[i];
+                    dp[i][j] = cost;
+                }
+            }
+        }
+        return dp[0][nsize-1];
     }
 };
