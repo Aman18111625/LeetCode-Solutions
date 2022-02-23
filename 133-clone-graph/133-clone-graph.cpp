8c86db691f63b8a5fa2e8c992a116bd0ce3cd952
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-23 09:56:45 +0530

    Time: 9 ms (46.46%), Space: 8.9 MB (45.65%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    void dfs(Node *curr,Node *node,vector<Node*>&vis)
+    {
+        vis[node->val]=node;
+        for(auto &elem : curr->neighbors)
+        {
+          if(!vis[elem->val])
+          {
+              Node *newNode=new Node(elem->val);
+              (node->neighbors).push_back(newNode);
+              dfs(elem,newNode,vis);
+          }
+          else
+          {
+              (node->neighbors).push_back(vis[elem->val]);
+          }
+        }
+    }
+public:
+    Node* cloneGraph(Node* node) {
+       if(!node) return node;
+       Node* copy= new Node(node->val);
+       vector<Node*>vis(101,NULL);
+       vis[copy->val]=copy;
+       for(auto &child : node->neighbors){
+         if(!vis[child->val]){
+           Node* temp=new Node(child->val);
+           (copy->neighbors).push_back(temp);
+           dfs(child,temp,vis);
+         }else{
+            (copy->neighbors).push_back(vis[child->val]);
+         }
+       }
+      return copy;
+    }
+};
