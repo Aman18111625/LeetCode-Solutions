1431ec9d91183ab74e4cef0fabc31d3b4556bfa7
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-24 23:20:17 +0530

    Time: 7 ms (5.92%), Space: 5.9 MB (61.56%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+    int maxDigit = -1;
+    int maxDigitIndex = -1;
+    int leftIndex = -1;
+    int rightIndex = -1;
+ 
+    string ans = to_string(num);
+    int size=ans.size();
+    for (int i = size - 1; i >= 0; i--) {
+        if (ans[i] > maxDigit) {
+            maxDigit = ans[i];
+            maxDigitIndex = i;
+          //  continue;
+        }
+        if (ans[i] < maxDigit) {
+            leftIndex = i;
+            rightIndex = maxDigitIndex;
+        }
+    }
+    if (leftIndex == -1) return num;
+    swap(ans[leftIndex], ans[rightIndex]);
+    return stoi(ans); 
+    }
+};
