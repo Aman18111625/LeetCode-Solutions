a120c7a005a957a0f1cdf90963956f3b6aa4e69d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-26 18:54:29 +0530

    Time: 456 ms (14.03%), Space: 28.5 MB (14.52%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n=graph.size();
+        if(n==1) return 0;
+        string allvisit(n,'1');
+        queue<pair<int,string>> q;
+        string cur(n,'0');
+        set<pair<int,string>> vis;
+        for(int i=0;i<n;i++){
+            cur[i]='1';
+            q.push({i,cur}); 
+            vis.insert({i,cur});
+            cur[i]='0';
+        }
+        
+        int step=0,v; char tempchr;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                v=q.front().first; cur=q.front().second; q.pop();
+                for(int i=0;i<graph[v].size();i++){
+                    tempchr=cur[graph[v][i]];
+                    cur[graph[v][i]]='1';
+                    if(cur==allvisit) return step+1;
+                    if(!vis.count({graph[v][i],cur})){q.push({graph[v][i],cur}); vis.insert({graph[v][i],cur});}
+                    cur[graph[v][i]]=tempchr;
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+};
+      
+     
+               
