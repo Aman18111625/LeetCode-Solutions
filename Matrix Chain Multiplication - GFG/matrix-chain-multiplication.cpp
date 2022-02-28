44f1f7ea338b05d15011c05a8091a3b26b127c0b
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-28 11:13:51 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -14,7 +14,7 @@ class Solution{
         if(i>=j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int mini=INT_MAX;
-        for(int k=i;k<=j-1;k++)
+        for(int k=i;k<j;k++)
         {
             int temp=solve(arr,i,k)+solve(arr,k+1,j)+arr[i-1]*arr[k]*arr[j];
             if(temp<mini) mini=temp;
