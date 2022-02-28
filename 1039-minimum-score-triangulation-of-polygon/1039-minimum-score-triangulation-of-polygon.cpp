5ab8593f94e4b64e1ee5908f5e4c9a10f6d4e2cb
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-28 11:35:03 +0530

    Time: 3 ms (97.67%), Space: 8 MB (99.10%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,5 +1,9 @@
 class Solution {
   //MCM -> Partition DP based Question
+  /*
+  //Recursive + Memoization
+  //TC:=>O(N*M)
+  //SC:=>O(N*M)+O(N)(stack-space)
   int dp[51][51];
   int helper(vector<int>&arr,int i,int j){
     if(i==j) return 0;
@@ -15,5 +19,23 @@ public:
         int n=values.size();
         memset(dp,-1,sizeof(dp));
         return helper(values,1,n-1);
+    }*/
+  public:
+    int minScoreTriangulation(vector<int>& arr) {
+        int n=arr.size();
+        int dp[n][n];
+        for(int i=0;i<n;i++) dp[i][i]=0;
+        for(int i=n-1;i>0;i--){
+          for(int j=i+1;j<n;j++){
+            int mini=INT_MAX;
+            for(int k=i;k<j;k++)
+            {
+              int temp=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+              mini=min(temp,mini);
+            }
+            dp[i][j]=mini;
+          }
+        }
+      return dp[1][n-1];
     }
 };
