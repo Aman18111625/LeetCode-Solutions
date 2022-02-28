467eb8e865a2d5b50244567939e16223f5ea2a28
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-28 16:17:38 +0530

    Added solution - LeetHub

--- Smallest window containing 0, 1 and 2 - GFG/smallest-window-containing-0-1-and-2.cpp
+++ Smallest window containing 0, 1 and 2 - GFG/smallest-window-containing-0-1-and-2.cpp
@@ -10,18 +10,22 @@ class Solution {
   public:
     int smallestSubstring(string S) {
         // Code here
-        int i=0,j=0,res=INT_MAX,n=S.size();
+        int i=0,j=-1,res=INT_MAX,n=S.size();
         int freq[3]={0};
-        while(j<n){
-            freq[S[j]-'0']++;
-            if(freq[0] && freq[1] && freq[2]){
-                while(freq[0] && freq[1] && freq[2]){
+        while(i<n){
+            while((!freq[0] or !freq[1] or !freq[2]) && j+1<n){
+                freq[S[j+1]-'0']++;
+                j++;
+            }
+            if(freq[0] && freq[1] && freq[2])  res=min(res,j-i+1);
+                if(j>=i){
                     freq[S[i]-'0']--;
                     i++;
+                }else{
+                    freq[S[i]-'0']--;
+                    i++;
+                    j=i-1;
                 }
-                res=min(res,j-(i-1)+1);
-            }
-            j++;
         }
         return res==INT_MAX?-1:res;
     }
