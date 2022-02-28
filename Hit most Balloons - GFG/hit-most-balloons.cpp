43c2825edaaf41de0d425815823b29d8eb35bf5b
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-28 10:37:22 +0530

    Added solution - LeetHub

---
+++ Hit most Balloons - GFG/hit-most-balloons.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+   double slope(pair<int, int> a, pair<int, int> b){
+    if (b.first - a.first == 0)
+        return 1510; // for infinite slope, it's just random value
+    return (b.second - a.second) / (double)(b.first - a.first);
+  }
+  public:
+    int mostBalloons(int N, pair<int, int> arr[]) {
+        // Code here
+        int ans = 0;
+        unordered_map<double, int> counter;
+        for (int i = 0; i < N; i++)
+        {
+            counter.clear();
+            int dup = 1;
+            for (int j = i + 1; j < N; j++)
+            {
+                if (arr[i] == arr[j]) // if pairs are same then increase duplicate count
+                    dup++;
+                else
+                    counter[slope(arr[i], arr[j])]++; // else add slope of those two points into the map
+            }
+            ans = max(ans, dup);
+            // count max number of points on a line when arr[i] is starting points and updating ans
+            for (auto p : counter)
+                ans = max(ans, p.second + dup);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        pair<int, int> arr[N];
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i].first;
+        }
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i].second;
+        }
+        Solution ob;
+        cout << ob.mostBalloons(N, arr) << endl;
+    }
+}  // } Driver Code Ends
