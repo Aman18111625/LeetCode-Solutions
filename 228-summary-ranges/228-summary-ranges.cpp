734d16f9a0b43abc26cd094bdffc7bb1a3b41d5e
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-02-28 10:18:14 +0530

    Time: 2 ms (36.96%), Space: 6.8 MB (62.02%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /*
     vector<string> summaryRanges(vector<int>& nums) {
          vector<string>ans;
          int n=nums.size();
@@ -20,5 +21,34 @@ public:
            j=i;
          }
       return ans;
-    }
+    }*/
+  vector<string> summaryRanges(vector<int>&nums){
+        int n=nums.size();
+        vector<string>ans;
+        if(n==0) return ans;
+        int start = nums[0], end = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                end = nums[i];
+            }
+            else {
+                if (start == end) {
+                    ans.push_back(to_string(end));
+                }
+                else {
+                    ans.push_back(to_string(start) + "->" + to_string(end));                 
+                }
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+       //checking for last range  
+        if (start == end) {
+            ans.push_back(to_string(end));
+        }
+        else {
+            ans.push_back(to_string(start) + "->" + to_string(end));             
+        }
+        return ans;
+  }
 };
