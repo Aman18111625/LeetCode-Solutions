7382447a203b64a1942359ad4186fdf558b96ecc
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-01 09:44:24 +0530

    Time: 14 ms (16.41%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,4 +1,5 @@
 class Solution {
+  /*TC:=>O(NLOGN)
   int countSetBit(int n){
     int cnt=0;
     while(n){
@@ -14,5 +15,15 @@ public:
           ans.push_back(countSetBit(i));
         }
       return ans;
+    }*/
+  public:
+  //TC:->O(N)
+  vector<int> countBits(int n){
+    vector<int>dp(n+1);
+    dp[0]=0;//number of 1's in 0 is 0
+    for(int i=1;i<=n;i++){
+      dp[i]=dp[i/2]+i%2;
     }
+    return dp;
+  }
 };
