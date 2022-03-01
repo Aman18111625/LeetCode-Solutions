1e2710e1c754310e7eee6b81eae36dcb7ee79c7e
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-01 17:03:24 +0530

    Time: 3 ms (73.38%), Space: 6.7 MB (85.39%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /*Approach-1 Using Queue
     vector<int> shortestToChar(string s, char c) {
         int n=s.size();
         vector<int>ans(n,0);
@@ -16,5 +17,25 @@ public:
            ans[i]=abs(curr-i);
         }
       return ans;
-    }
+    }*/
+   vector<int> shortestToChar(string s, char c){
+     int n=s.size();
+     vector<int>ans(n,INT_MAX);
+     int p=INT_MAX;
+     for(int i=0;i<n;i++){
+       if(s[i]==c){
+         p=i;
+         ans[i]=0;
+       }else{
+         ans[i]=abs(p-i);
+       }
+     }
+     for(int i=n-1;i>=0;i--){
+        if (s[i] == c)
+            p = i;
+        else
+            ans[i] = min(ans[i], abs(p - i));
+     }
+     return ans;
+   }
 };
