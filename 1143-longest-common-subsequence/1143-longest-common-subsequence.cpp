38f137b474930cd6a958cf7b1a34d98abe687e46
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-02 11:10:49 +0530

    Time: 8 ms (97.41%), Space: 6.4 MB (98.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -22,6 +22,10 @@ public:
       return helper(text1,text2,n,m,dp);
     }*/
     public:
+  /*
+  //Bottom-Up
+  //TC:=>O(N*M)
+  //SC:=>O(N*M)
     int longestCommonSubsequence(string text1, string text2) {
       int n=text1.size(),m=text2.size();
       vector<vector<int>>dp(n+1,vector<int>(m+1));
@@ -36,5 +40,21 @@ public:
          }
        }
       return dp[n][m];
+    }*/
+  //Space-Optimization
+    int longestCommonSubsequence(string text1, string text2) {
+      int n=text1.size(),m=text2.size();
+         vector<int>prev(m+1,0),curr(m+1,0);
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=m;j++){
+           if(text1[i-1]==text2[j-1]){
+              curr[j]=1+prev[j-1];
+            }else{
+              curr[j]=max(prev[j],curr[j-1]);
+            }
+         }
+         prev=curr;
+       }
+      return prev[m];
     }
 };
