8f4ff4ee0ba17c98256f5fded2bb8909c2d45545
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-06 16:56:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int countOrders(int n) {
-      /*
         if(n==1) return 1;
-        int res=1;
+        int mod=1e9+7;
+        long long res=1;
         for(int i = 1; i <=n; ++i)
         {
 		   	// calulating n!
             res *= i;
 			// to avoid overflow
-    //        res %= mod;
+           res %= mod;
 			
 			// (2*i-1) are the vacant places which we can choose
 			// later we have n! permutation of pickup task
 			// therefore we have to multiply with n! that we calculated as res.
             res *= (2*i-1);
-			
 			// to avoid overflow
-          //  res %= mod;
+           res %= mod;
         }
-      return res;*/
-      int mod=1e9+7;
+      return res;
+    /*TC:=>O(N) SC:=>O(N)
       vector<long long>dp(n);
       dp[0]=1;
       for(int i=1;i<n;i++){
@@ -28,6 +27,7 @@ public:
         long long ways = (gaps*gaps)-(gaps*(gaps-1))/2;
         dp[i]=(dp[i-1]*ways)%mod;
       }
-      return dp[n-1];
+      return dp[n-1];*/
+        
     }
 };
