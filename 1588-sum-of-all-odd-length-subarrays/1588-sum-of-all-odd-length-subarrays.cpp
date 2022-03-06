fe0f3d14dc20b0ba7317613c76b134d48d09e064
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-06 16:10:39 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (81.62%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+  //Approach-1 generate all the sub-array & check for every odd length subarray
     int sumOddLengthSubarrays(vector<int>& arr) {
         int n=arr.size(),sum=0;
         for(int i=0;i<n;i++){
-            int curr=0,len=0;
+            int curr=0,len=1;
             for(int j=i;j<n;j++){
                 curr+=arr[j];
-                if(len%2==0){
+                if(len&1){
                     sum+=curr;
                 }
                 len++;
