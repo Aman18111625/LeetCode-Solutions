52f1f2ece752daa5ae91774065fb3c27c53e3b4a
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-08 11:17:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (56.90%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  /*
     string interpret(string command) {
         unordered_map<string,string>mpp;
         mpp["(al)"]="al";
@@ -14,5 +15,22 @@ public:
           }
         }
         return res;
+    }*/
+     string interpret(string command) {
+        string s="",str;
+        for(char ch: command){
+            s+=ch;
+            if(s=="G"){
+                str+="G";
+                s="";
+            }else if(s=="()"){
+                str+="o";
+                s="";
+            }else if(s=="(al)"){
+                str+="al";
+                s="";
+            }
+        }
+        return str;
     }
 };
