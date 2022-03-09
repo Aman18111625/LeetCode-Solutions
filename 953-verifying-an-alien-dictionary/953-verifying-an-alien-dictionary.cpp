9cae95e9fbbf701fb17adb0c2ddffa445a9fb9d6
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-09 11:14:16 +0530

    Time: 3 ms (93.33%), Space: 9.5 MB (79.35%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -2,18 +2,17 @@ class Solution {
   unordered_map<int,int>mpp;
   bool helper(string &a,string &b){
     int i=0;
-    while(i<a.size() && i<b.size()){
+    while(i<size(a) && i<size(b)){
       if(mpp[a[i]]!=mpp[b[i]]) return mpp[a[i]]<mpp[b[i]];
       i++;
     }
-    return a.size()<=b.size();
+    return size(a)<=size(b);
   }
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        int size=order.size(),n=words.size();
-        for(int i=0;i<size;i++) mpp[order[i]]=i;//mapping letter to its corresponding index
+        for(int i=0;i<size(order);i++) mpp[order[i]]=i;//mapping letter to its corresponding index
       //now check two words are lexicographically sorted
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<size(words)-1;i++){
           if(!helper(words[i],words[i+1])) return false;
         }
       return true;
