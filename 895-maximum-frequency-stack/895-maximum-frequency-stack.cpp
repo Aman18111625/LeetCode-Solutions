3b7520370faa4e8d1f8e8e0602908bb7d510eb0c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-19 11:27:09 +0530

    Time: 221 ms (76.02%), Space: 79.1 MB (93.90%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -0,0 +1,25 @@
+class FreqStack {
+    priority_queue<pair<int, pair<int, int>>> q;//{freq,{elem,pos}}
+  //using max heap because we have to perform operations on most freq element
+    unordered_map<int,int> freq;
+    int pos = 0;
+public:
+    void push(int x) {
+        q.emplace(++freq[x], make_pair(++pos, x));
+    }
+    
+    int pop() {
+        auto val = q.top();
+        q.pop();
+        int x = val.second.second;
+        freq[x]--;
+        return x;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
