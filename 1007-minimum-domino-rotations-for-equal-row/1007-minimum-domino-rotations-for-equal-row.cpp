b93cfba18e5fd498af00eed230e933ae21060d4a
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-03-20 10:44:46 +0530

    Time: 162 ms (69.36%), Space: 111.6 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,4 +1,5 @@
 class Solution {
+  /*
       int countDifference(vector<int>& A, vector<int>& B, int& num) {
         int countA = 0, countB = 0;
         int n=A.size();
@@ -15,5 +16,30 @@ public:
        int res1=countDifference(tops,bottoms,tops[0]);
        int res2=countDifference(tops,bottoms,bottoms[0]);
        return min(res1,res2)>0?min(res1,res2):max(res1,res2);
+    }*/
+  public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int n = tops.size();  
+        // faceA for counting the occurence in tops
+    		// faceB for counting occurence of numbers in bottoms
+        // same for counting when both tops and bottoms have same occurences
+        vector<int> faceA(7), faceB(7), same(7);  
+        for(int i = 0; i < n; ++i)
+        {
+            ++faceA[tops[i]];
+            ++faceB[bottoms[i]];
+            if(tops[i] == bottoms[i])
+                ++same[tops[i]];
+        }
+        int minRotation = INT_MAX; 
+		    // all possibilities from 1 to 6
+        for(int i = 1; i<=6; ++i)
+        {
+            if(faceA[i] + faceB[i] - same[i] == n)
+                minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
+        }
+        
+        return minRotation == INT_MAX ? -1 : minRotation;
+        
     }
 };
