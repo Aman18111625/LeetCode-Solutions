bc66603600fde70437db827efdd262359af7a168
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-08 12:28:30 +0530

    Time: 309 ms (16.56%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
+/*
 select IFNULL(
     Null, 
     (select distinct Salary from Employee 
      order by Salary desc limit 1 offset 1)
  )
-as SecondHighestSalary
+as SecondHighestSalary*/
+Select Max(sal) as SecondHighestSalary  from (select Salary as sal, Rank() OVER (order by salary desc) as Rnk from Employee) T where Rnk=2 ;
