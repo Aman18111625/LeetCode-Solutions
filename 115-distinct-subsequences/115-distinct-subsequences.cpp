33c24dcde954bac38eb6ed1a3283d96454111049
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-09 23:59:28 +0530

    Time: 42 ms (61.79%), Space: 6.4 MB (97.02%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -19,6 +19,7 @@ public:
       return helper(s,t,m-1,n-1);
     }*/
   public:
+  /*
     int numDistinct(string s, string t){
         int m=s.length();
         int n=t.length();
@@ -33,5 +34,21 @@ public:
             }
         }
         return dp[m][n];
-    }
+    }*/
+   int numDistinct(string s, string t){
+     int n=s.size(),m=t.size();
+     vector<double>prev(m+1,0),curr(m+1,0);
+     prev[0]=curr[0]=1;
+     for(int i=1;i<=n;i++){
+       for(int j=1;j<=m;j++){
+         if(s[i-1]==t[j-1]){
+           curr[j]=prev[j-1]+prev[j];
+         }else{
+           curr[j]=prev[j];
+         }
+       }
+       prev=curr;
+     }
+     return prev[m];
+   }
 };
