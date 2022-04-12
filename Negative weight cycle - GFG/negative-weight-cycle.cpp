04a91e4594f29b8f10979b74dae94b482a171312
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-12 09:10:33 +0530

    Added solution - LeetHub

--- Negative weight cycle - GFG/negative-weight-cycle.cpp
+++ Negative weight cycle - GFG/negative-weight-cycle.cpp
@@ -7,6 +7,7 @@ class Solution {
 public:
 	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
 	    // Code here
+	    /*
 	    const int inf=1e7;
 	    vector<int>dist(n,inf);
 	    dist[0]=0;
@@ -23,8 +24,28 @@ public:
 	            dist[v]=min(dist[v],dist[u]+w);
 	        }
 	    }
-	    return (change==true)?1:0;
-	}
+	    return (change==true)?1:0;*/
+	     int dis[n];
+         dis[0] = 0;
+        for(int i = 1; i < n; i++) dis[i] = INT_MAX;
+    
+        for(int cnt = 1; cnt < n; cnt++){
+          for(auto ed: edges){
+            int u = ed[0];
+            int v = ed[1];
+            int w = ed[2];
+            if(dis[u] != INT_MAX && dis[u] + w < dis[v]) dis[v] = dis[u] + w;
+          }
+        }
+    
+        for(auto ed: edges){
+          int u = ed[0];
+          int v = ed[1];
+          int w = ed[2];
+          if(dis[u] != INT_MAX && dis[u] + w < dis[v]) return (1);
+        }
+      return 0;
+	 }
 };
 
 // { Driver Code Starts.
