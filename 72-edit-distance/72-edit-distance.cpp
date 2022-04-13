188fc9dac7d8ef21f3156fd5b813418e39a33c67
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-13 22:18:31 +0530

    Time: 16 ms (61.35%), Space: 6.5 MB (93.44%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,4 +1,6 @@
 class Solution {
+  /*
+  //Recursive + Memoization
   int dp[501][501];
   int helper(string& a,string& b,int i,int j)
     {
@@ -15,5 +17,43 @@ public:
     int minDistance(string word1, string word2) {
         memset(dp,-1,sizeof(dp));
         return helper(word1,word2,0,0);
+    }*/
+  public:
+  /*tabulation
+    int minDistance(string a, string b){
+      int n=a.size(),m=b.size();
+      vector<vector<int>>dp(n,vector<int>(m,0));
+      for(int i=0;i<=n;i++) dp[i][0]=i;
+      for(int j=0;j<=m;j++) dp[0][j]=j;
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+          if(a[i-1]==b[j-1]){
+            dp[i][j]=dp[i-1][j-1];
+          }else{
+            dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+          }
+        }
+      }
+      return dp[n];
+    }*/
+  int minDistance(string a, string b){
+      int n=a.size(),m=b.size();
+      vector<int>prev(m+1,0),curr(m+1,0);
+      for(int i=0;i<=m;i++){
+        prev[i]=i;
+      }
+    
+      for(int i=1;i<=n;i++){
+        curr[0]=i;
+        for(int j=1;j<=m;j++){
+          if(a[i-1]==b[j-1]){
+            curr[j]=prev[j-1];
+          }else{
+            curr[j]=1+min(prev[j],min(curr[j-1],prev[j-1]));
+          }
+        }
+        prev=curr;
+      }
+      return prev[m];
     }
 };
