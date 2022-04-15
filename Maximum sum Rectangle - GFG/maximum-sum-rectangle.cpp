47c89249df71478ed0e3e6e6d145bcdfca3725b1
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-15 11:17:35 +0530

    Added solution - LeetHub

--- Maximum sum Rectangle - GFG/maximum-sum-rectangle.cpp
+++ Maximum sum Rectangle - GFG/maximum-sum-rectangle.cpp
@@ -11,20 +11,20 @@ class Solution {
   public:
     int maximumSumRectangle(int R, int C, vector<vector<int>> M) {
         // code here
-       int arr[C];
-       int max_so_far=INT_MIN;
+      
+       int ans=INT_MIN;
        for(int i=0;i<R;i++){
-           memset(arr,0,sizeof(arr));
+            vector<int>arr(C,0);
            for(int j=i;j<R;j++){
                int sum=0;
                for(int k=0;k<C;k++){
                    arr[k]+=M[j][k];
                    sum=max(sum+arr[k],arr[k]);
-                   max_so_far=max(sum,max_so_far);
+                   ans=max(sum,ans);
                }
            }
        }
-       return max_so_far;
+       return ans;
     }
 };
 
