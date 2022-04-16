d0326c5c807a7822b8c9555cefe0c874ac8eb5b7
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-16 09:48:41 +0530

    Added solution - LeetHub

--- Word Wrap - GFG/word-wrap.cpp
+++ Word Wrap - GFG/word-wrap.cpp
@@ -8,6 +8,7 @@ using namespace std;
 //User function Template for C++
 
 class Solution {
+    /*
     int helper(vector<int>&nums,int ind,int limit,int rl,vector<vector<int>>&dp){
         if(ind==nums.size()-1){
             if(nums[ind]<rl) return dp[ind][rl]=0;
@@ -29,6 +30,30 @@ public:
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(k+1,-1));
         return helper(nums,0,k,k,dp);
+    } */
+    public:
+    int solveWordWrap(vector<int>nums, int k){  
+        vector<vector<int>> dp(nums.size(),vector<int>(k+1,0));
+        int n=nums.size();
+        for(int i=0;i<=nums[n-1];i++)
+         dp[n-1][i]=i*i;
+         for(int ind=n-2;ind>=0;ind--)
+         {
+             for(int rl=0;rl<=k;rl++)
+             {
+                   if(nums[ind]<rl)
+                   {
+                       int take=dp[ind+1][rl==k?rl-nums[ind]:rl-nums[ind]-1];
+                       int notTake=rl*rl+dp[ind+1][k-nums[ind]];
+                       dp[ind][rl] = min(take,notTake);
+                   }
+                   else
+                   {
+                       dp[ind][rl] = rl*rl+dp[ind+1][k-nums[ind]];
+                   }
+             }
+         }
+         return dp[0][k];
     } 
 };
 
