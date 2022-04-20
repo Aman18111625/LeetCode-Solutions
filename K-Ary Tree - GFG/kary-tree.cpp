6d90fd64b46fb84563928b836ee7d7df05da87c2
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-20 19:38:43 +0530

    Added solution - LeetHub

--- K-Ary Tree - GFG/kary-tree.cpp
+++ K-Ary Tree - GFG/kary-tree.cpp
@@ -9,13 +9,27 @@ using namespace std;
 
 class Solution {
   public:
-    long long karyTree(int k, int m) {
-        // code here
-        long long int  leaf=1;
-        for(int i=0;i<m;i++){
-            leaf=(leaf*k)%1000000007;
-        }
-        return leaf;
+    //Time-Complexity:=> K*M
+    // long long karyTree(int k, int m) {
+    //     // code here
+    //     long long int  leaf=1;
+    //     for(int i=0;i<m;i++){
+    //         leaf=(leaf*k)%1000000007;
+    //     }
+    //     return leaf;
+    // }
+    long long mod=1e9+7;
+    long long recursive(int k,int m){
+      long long temp;
+      if(m==0) return 1;
+      temp=recursive(k,m/2)%mod;
+      if(m%2==0) return (temp*temp)%mod;
+      return (k%mod* temp%mod* temp%mod)%mod;
+    }
+    long long karyTree(int k, int m){
+       long long ans=0;
+       ans=recursive(k,m)%mod;
+       return ans;
     }
 };
 
