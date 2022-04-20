ee3e30cbb07c5bce1f9ca1b9fb175cf57e59010c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-20 19:04:00 +0530

    Time: 21 ms (39.69%), Space: 13.3 MB (54.71%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
   //Brute-Force
+  //Time-Complexity:=>O((N*M)(N+M))
+  //Space-Complexity:=>O(1)
   /*
     void setZeroes(vector<vector<int>>& mat) {
         int n=mat.size();
@@ -37,6 +39,10 @@ public:
           }
         }
     }*/
+    //Better Approach
+    //Time-Complexity:->O(N*M)
+    //Space-Complexity:->O(N)+O(M)
+  /*
     void setZeroes(vector<vector<int>>& mat){
       int n=mat.size();
       int m=mat[0].size();
@@ -56,4 +62,29 @@ public:
         }
       }
     }
+    */
+  //Optimized Approach
+  //Time-Complexity:->O(N*M)
+  //SPace-Compleixty:->O(1)
+  void setZeroes(vector<vector<int>>& mat){
+    int n=mat.size(),m=mat[0].size();
+    int col0=1;
+    for(int i=0;i<n;i++){
+      if(mat[i][0]==0) col0=0;
+      for(int j=1;j<m;j++){
+        if(mat[i][j]==0){
+          mat[i][0]=0;
+          mat[0][j]=0;
+        }
+      }
+    }
+    for(int i=n-1;i>=0;i--){
+      for(int j=m-1;j>=1;j--){
+        if(mat[i][0]==0 or mat[0][j]==0){
+          mat[i][j]=0;
+        }
+      }
+      if(col0==0) mat[i][0]=0; 
+    }
+  }
 };
