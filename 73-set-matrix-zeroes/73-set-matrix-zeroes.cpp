cc4ae89469d7a94a7b71ed9a3777dd2edf44e506
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-20 18:39:21 +0530

    Time: 17 ms (59.42%), Space: 13.2 MB (54.71%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  //Brute-Force
+    void setZeroes(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+            if(mat[i][j]==0){
+               int row=i-1;
+               while(row>=0){
+                 if(mat[row][j]!=0) mat[row][j]=1e9;
+                 row--;
+               }
+               row=i+1;
+               while(row<n){
+                 if(mat[row][j]!=0) mat[row][j]=1e9;
+                 row++;
+               }
+               int col=j-1;
+               while(col>=0){
+                 if(mat[i][col]!=0)  mat[i][col]=1e9;
+                 col--;
+               }
+               col=j+1;
+               while(col<m){
+                 if(mat[i][col]!=0) mat[i][col]=1e9;
+                 col++;
+               }
+            }
+          }
+        }
+        for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+            if(mat[i][j]==1e9) mat[i][j]=0;
+          }
+        }
+    }
+};
