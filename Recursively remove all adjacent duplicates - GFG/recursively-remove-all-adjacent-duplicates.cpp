9f6506d38b8ee295fab32ef9cf7b4a6a11c90321
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-23 11:12:12 +0530

    Added solution - LeetHub

--- Recursively remove all adjacent duplicates - GFG/recursively-remove-all-adjacent-duplicates.cpp
+++ Recursively remove all adjacent duplicates - GFG/recursively-remove-all-adjacent-duplicates.cpp
@@ -9,49 +9,72 @@ using namespace std;
 
 class Solution{
 public:
-    string remove(string s){
-        // code here
-        /*
-        while(1){
-            int n=s.size(),i=0;
-            string temp="";
-            while(i<n){
-                int j=i;
-                while(++j<n && s[i]==s[j]);
-                if(j==i+1) temp+=s[i];
-                i=j;
+    string rem(string s){
+        int n=s.size();
+        int i=0;
+        string ans="";
+        while(i<n){
+            if(i<n-1 && s[i]==s[i+1]){
+               while(i<n-1 && s[i]==s[i+1]) i++; 
+            }else{
+                ans+=s[i];
             }
-            if(s.size()==temp.size()) break;
-            s=temp;
+            i++;
         }
-        return s;*/
-      int n=s.size();
-      if(n<=1) return s;
-      int cnt=0;//store how many different character
-      for(int i=0;i<n-1;i++){
-           if(s[i]!=s[i+1]) cnt++;
-       }
-       if(cnt==n-1) return s;//now there is no adjacent duplicate character
-       int i=0;
-       string ans="";
-       while(i<n-1){
-           if(s[i]!=s[i+1]){
-               ans.push_back(s[i]);
-               i++;
-               continue;
-           }
-           int st=i;
-           //remove duplicats
-           while(i<n and s[i]==s[st])i++;
-       }
-       //for last character
-        if(i==n-1){
-          ans.push_back(s[i]);
-          i++;
-        }
-        //now again  check for string ans 
-        return remove(ans);
+        return ans;
     }
+
+    string remove(string s){
+        string s1;
+        while(s.size()!=s1.size()){
+            s1=s;
+            s=rem(s);
+        }
+        return s;
+    }
+    // string remove(string s){
+    //     // code here
+    //     /*
+    //     while(1){
+    //         int n=s.size(),i=0;
+    //         string temp="";
+    //         while(i<n){
+    //             int j=i;
+    //             while(++j<n && s[i]==s[j]);
+    //             if(j==i+1) temp+=s[i];
+    //             i=j;
+    //         }
+    //         if(s.size()==temp.size()) break;
+    //         s=temp;
+    //     }
+    //     return s;*/
+    //   int n=s.size();
+    //   if(n<=1) return s;
+    //   int cnt=0;//store how many different character
+    //   for(int i=0;i<n-1;i++){
+    //       if(s[i]!=s[i+1]) cnt++;
+    //   }
+    //   if(cnt==n-1) return s;//now there is no adjacent duplicate character
+    //   int i=0;
+    //   string ans="";
+    //   while(i<n-1){
+    //       if(s[i]!=s[i+1]){
+    //           ans.push_back(s[i]);
+    //           i++;
+    //           continue;
+    //       }
+    //       int st=i;
+    //       //remove duplicats
+    //       while(i<n and s[i]==s[st])i++;
+    //   }
+    //   //for last character
+    //     if(i==n-1){
+    //       ans.push_back(s[i]);
+    //       i++;
+    //     }
+    //     //now again  check for string ans 
+    //     return remove(ans);
+    // }
 };
 
 // { Driver Code Starts.
