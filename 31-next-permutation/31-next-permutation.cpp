b1883fdf9f35f822b53fd7dc796fc083d1edded7
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-23 11:52:33 +0530

    Time: 4 ms (72.76%), Space: 12.1 MB (37.98%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -5,19 +5,23 @@ public:
         int k,l;
         for(k=n-2;k>=0;k--)
         {
+          //1.find ind1 from back such a[i]<a[i+1]
         if(nums[k]<nums[k+1]) break; 
         }
+        //if permutation is like 3,2,1 . this is last permutation of 1,2,3 so next greater is not possible so we'll return reverse of it.
         if(k<0)
         {
-          reverse(nums.begin(),nums.end());
+          reverse(nums.begin(),nums.end());//edge-case
         }
         else
         {
-        for(l=n-1;l>k;l--)
-        {
-         if(nums[l]>nums[k]) break;
-         }
-        swap(nums[k],nums[l]);
+          //2.find ind2 from back such a[ind2]>a[ind1]
+           for(l=n-1;l>k;l--){
+            if(nums[l]>nums[k]) break;
+           }
+          //3.swap a[ind1],a[ind2]
+            swap(nums[k],nums[l]);
+          //4. reverse all after ind1 because we have to return next greater so rank will be lesser as much as possible
             reverse(nums.begin()+k+1,nums.end());
        }
     }
