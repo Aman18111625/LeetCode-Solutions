2bd8d1e4de0133433e895d1f9233f16828b01dff
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-25 18:53:02 +0530

    Time: 9 ms (7.13%), Space: 7.1 MB (74.12%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,19 +2,35 @@ class Solution {
 public:
   //Approach-1 Brute Force
   //Time-Complexity:=>O(N^2) Space-Complexity:=>O(N^2)
-    void rotate(vector<vector<int>>& mat) {
+//     void rotate(vector<vector<int>>& mat) {
+//          int n=mat.size();
+//          vector<vector<int>>ans(n,vector<int>(n,0));
+//          for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//              ans[j][n-i-1]=mat[i][j];
+//            }
+//          }
+      
+//          for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//              mat[i][j]=ans[i][j];
+//            }
+//          }
+      
+//     }
+  //Approach-2 Optimized Approach
+  //step-1 Transpose of given matrix so that row will become column and hence column will become row
+  //Step-2 Now reverse each row
+   void rotate(vector<vector<int>>& mat) {
          int n=mat.size();
-         vector<vector<int>>ans(n,vector<int>(n,0));
          for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-             ans[j][n-i-1]=mat[i][j];
+           for(int j=0;j<i;j++){
+              swap(mat[i][j],mat[j][i]);
            }
          }
       
          for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-             mat[i][j]=ans[i][j];
-           }
+           reverse(mat[i].begin(),mat[i].end());
          }
       
     }
