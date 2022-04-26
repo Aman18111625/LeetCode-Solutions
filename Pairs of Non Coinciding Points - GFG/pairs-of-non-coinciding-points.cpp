82b630106c429b306d44f1e39841176b3cfde497
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-26 19:01:47 +0530

    Added solution - LeetHub

--- Pairs of Non Coinciding Points - GFG/pairs-of-non-coinciding-points.cpp
+++ Pairs of Non Coinciding Points - GFG/pairs-of-non-coinciding-points.cpp
@@ -10,6 +10,7 @@ class Solution {
   public:
     int numOfPairs(int X[], int Y[], int n) {
         // code here
+        //if x1==x2 then Eucliean Distance and Manhattan Distance will be equal
         map<int,int>map1;
         int cnt1=0;//counting pairs having x coordinate as qual
         for(int i=0;i<n;i++){
@@ -18,24 +19,25 @@ class Solution {
             }
             map1[X[i]]++;
         }
-        
+        //if y1==y2 then Eucliean Distance and Manhattan Distance will be equal
         map<int,int>map2;
-        int cnt2=0;
+        int cnt2=0;//counting pairs having y coordinate as equal
         for(int i=0;i<n;i++){
             if(map2.find(Y[i])!=map2.end()){
                 cnt2+=map2[Y[i]];
             }
             map2[Y[i]]++;
         }
-        
+        //if x1==x2 & y1==y2 then point will be coincides 
         map<pair<int,int>,int>map3;
-        int cnt3=0;
+        int cnt3=0;//counting pairs having both x & y  coordinate as equal
         for(int i=0;i<n;i++){
             if(map3.find({X[i],Y[i]})!=map3.end()){
                 cnt3+=map3[{X[i],Y[i]}];
             }
             map3[{X[i],Y[i]}]++;
         }
+        //for result we need to - cnt3 two times because same points already counted in cnt1 & cnt2
         return cnt1+cnt2-2*cnt3;
     }
 };
