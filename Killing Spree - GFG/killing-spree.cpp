a506e08afa878b162063d27d191607b53996dec4
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-28 19:00:48 +0530

    Added solution - LeetHub

--- Killing Spree - GFG/killing-spree.cpp
+++ Killing Spree - GFG/killing-spree.cpp
@@ -9,6 +9,7 @@ public:
     long long int killinSpree(long long int n)
     {
         // Code Here
+        /*
         long long int cnt=0;
         int i=1;
         while(n){
@@ -19,6 +20,21 @@ public:
             i++;
         }
         return cnt;
+        */
+        long long int low=1,high=1e6;
+        long long int mid,sum=0,ans=0;
+        while(low<=high){
+            mid=(low)+(high-low)/2;
+            sum=mid*1LL*(mid+1)*(2*mid+1);
+            sum/=6;
+            if(sum<=n){
+                low=mid+1;
+                ans=mid;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
     }
 };
 
