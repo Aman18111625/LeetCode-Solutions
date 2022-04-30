11da1b51eeef5d812f49e40ff8196c3c7d4a5e96
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-04-30 11:21:21 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (74.56%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
 void createAdjList(unordered_map<string,unordered_map<string,double>>& adj,vector<vector<string>>&equations,vector<double>&values){
-        int n=equations.size();
-        for(int i=0;i<n;i++){
-            string from=equations[i][0];
-            string to=equations[i][1];
+         int i=0;
+         for(auto &it : equations){
+            string from=it[0];
+            string to=it[1];
             adj[from].insert({to,values[i]});
             adj[to].insert({from,(double)1/values[i]});
+            i++;
         }
     }
    
@@ -17,7 +18,7 @@ void createAdjList(unordered_map<string,unordered_map<string,double>>& adj,vecto
        unordered_map<string,unordered_map<string,double>>adj;
        createAdjList(adj,equations,values);
        //calculate res for each query
-       for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
          unordered_set<string>vis;
          ans[i]=dfs(q[i][0],q[i][1],vis,adj);
        }
@@ -25,9 +26,9 @@ void createAdjList(unordered_map<string,unordered_map<string,double>>& adj,vecto
     }
     
     double dfs(string s,string d,unordered_set<string>&vis,unordered_map<string,unordered_map<string,double>>&adj){
-      if(adj.find(s)==adj.end()) return -1.0;
+      if(adj.find(s)==adj.end()) return -1.0;//if src is not found ->return -1.0
       if(adj[s].find(d)!=adj[s].end()) return adj[s][d];
-      vis.insert(s);
+      vis.insert(s);//mark as visited
       //explore all the paths
       for(auto &it : adj[s]){
         if(!vis.count(it.first)){
