f6b9cb84edfc97673fa2a3b49e0d6aefef0ca997
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-01 11:17:57 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.67%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string res1="",res2="";
-        for(auto ch : s){
-          if(islower(ch)) res1+=ch;
-          else if(!res1.empty()) res1.pop_back();
+       //Brute-Force 
+       //TC:=>O(N) ,SC:=>O(N)
+//       string res1="",res2="";
+//       for(auto ch : s){
+//         if(islower(ch)) res1+=ch;
+//         else if(!res1.empty()) res1.pop_back();
+//       }
+
+//       for(auto ch : t){
+//         if(islower(ch)) res2+=ch;
+//         else if(!res2.empty()) res2.pop_back();
+//       }
+//       return res1==res2;
+        int k=0,p=0;
+        int n=s.size(),m=t.size();
+        for(int i=0;i<n;i++){
+          if(s[i]=='#') {
+            k--;
+            k=max(0,k);
+          }else{
+            s[k++]=s[i];
+          }
         }
-        
-        for(auto ch : t){
-          if(islower(ch)) res2+=ch;
-          else if(!res2.empty()) res2.pop_back();
+        for(int i=0;i<m;i++){
+          if(t[i]=='#') {
+            p--;
+            p=max(0,p);
+          }else{
+            t[p++]=t[i];
+          }
         }
-        return res1==res2;
+       if(k!=p) return false;
+       else{
+         for(int i=0;i<k;i++) if(s[i]!=t[i]) return false;
+       }
+        return true;
     }
 };
