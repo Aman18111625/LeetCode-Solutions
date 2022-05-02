699337bb6c3b0fcc947e3ae9048b0f1efd0b2599
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-02 08:25:51 +0530

    Time: 20 ms (24.29%), Space: 16.2 MB (83.25%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
+      /*Brute Force
         vector<int>odd,even;
         for(auto num : nums) {
           if(num%2)odd.push_back(num);
@@ -11,6 +12,15 @@ public:
         for(auto it : even) nums[i++]=it;
         for(auto it : odd) nums[i++]=it;
         
+        return nums;*/
+       int j=0;
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+         if(nums[i]%2==0){
+           swap(nums[i],nums[j]);
+           j++;
+         }
+       }
         return nums;
     }
 };
