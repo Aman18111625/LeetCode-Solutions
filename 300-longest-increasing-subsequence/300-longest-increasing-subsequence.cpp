bef324659620a4785b301bb0f4773b218cf164d1
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-04 23:23:20 +0530

    Time: 525 ms (15.20%), Space: 10.6 MB (18.28%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -20,33 +20,35 @@ public:
 //         vector<vector<int>>dp(n,vector<int>(n+1,-1));
 //         return solve(v,0,-1,n,dp);
 //     }
-       int lengthOfLIS(vector<int>& v) {
-         int n=v.size();
-         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-         for(int ind=n-1;ind>=0;ind--){
-           for(int prev=ind-1;prev>=-1;prev--){
-             int len=dp[ind+1][prev+1];
-             if(prev==-1 or v[ind]>v[prev]){
-               len=max(len,1+dp[ind+1][ind+1]);
-             }
-             dp[ind][prev+1]=len;
-           }
-         }
-        return dp[0][0];
-       }
-  /*
+      //Tablulation:=> TC:=>O(N*N) SC:=>O(N*N)
+       // int lengthOfLIS(vector<int>& v) {
+       //   int n=v.size();
+       //   vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+       //   for(int ind=n-1;ind>=0;ind--){
+       //     for(int prev=ind-1;prev>=-1;prev--){
+       //       int len=dp[ind+1][prev+1];
+       //       if(prev==-1 or v[ind]>v[prev]){
+       //         len=max(len,1+dp[ind+1][ind+1]);
+       //       }
+       //       dp[ind][prev+1]=len;
+       //     }
+       //   }
+       //  return dp[0][0];
+       // }
   //TC:=>O(N^2) SC:=>O(N)
    int lengthOfLIS(vector<int>& v) {
         int n=v.size();
-        vector<int>dp(n);
-        dp[0]=1;
-        for(int i=1;i<n;i++){
-          dp[i]=1;
-          for(int j=0;j<i;j++){
-            if(v[i]>v[j]) dp[i]=max(dp[i],1+dp[j]);
+        vector<int>next(n+1,0),curr(n+1,0);
+        for(int ind=n-1;ind>=0;ind--){
+          for(int prev=ind-1;prev>=-1;prev--){
+            int len=next[prev+1];
+            if(prev==-1 or v[ind]>v[prev]){
+              len=max(len,1+next[ind+1]);
+            }
+            curr[prev+1]=len;
           }
+          next=curr;
         }
-       return *max_element(dp.begin(),dp.end());
+        return next[0];
     }
- */
 };
