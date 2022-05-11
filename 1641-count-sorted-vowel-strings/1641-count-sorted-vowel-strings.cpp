747da5fc6187fb38f9b64ef1190977cf04beac25
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-11 21:25:18 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    int countVowelStrings(int n) {
-       if(n==1) return 5;
-       int a=1, e=1, i=1, o=1, u=1;  
-        while(--n){
-            o += u;
-            i += o;
-            e += i;
-            a += e;
-        }
+  //Approach-2 Without Using Extra Space
+  //Time:=>O(N)
+  //Space:=>O(1)
+//     int countVowelStrings(int n) {
+//        if(n==1) return 5;
+//        int a=1, e=1, i=1, o=1, u=1;  
+//         while(--n){
+//             o += u;
+//             i += o;
+//             e += i;
+//             a += e;
+//         }
         
-        return a+e+i+o+u;
+//         return a+e+i+o+u;
+//     }
+  //Approach-1 Using DP
+  //Time:=>O(N)
+  //Space:=>O(N)
+   int countVowelStrings(int n) {
+        vector<int> dp(5, 1);
+        int ans = 0;
+        while(--n){
+            for(int i=3; i>=0; i--){
+                dp[i] += dp[i+1];
+            }
+        }
+        for(auto x:dp) ans += x;
+        return ans;
     }
 };
