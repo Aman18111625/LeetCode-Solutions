905c4dd4234bc264bdef9b447f4ea6f09b8e2795
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-15 10:24:35 +0530

    Time: 110 ms (80.06%), Space: 59.8 MB (53.32%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,23 +11,35 @@
  */
 class Solution {
 public:
-    int deepestLeavesSum(TreeNode* root) {
-        if(!root) return 0;
-        if(!root->left && !root->right) return root->val;
-        queue<TreeNode*>q;  
-        q.push(root);
-        int sum=0;
-        while(!q.empty()){
-          int size=q.size();
-          sum=0;
-          for(int i=0;i<size;i++){
-            TreeNode* node=q.front();
-            q.pop();
-            sum+=node->val;
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-          }
-        } 
-         return sum;
-    }
+    //BFS APPROACH
+    // int deepestLeavesSum(TreeNode* root) {
+    //     if(!root) return 0;
+    //     if(!root->left && !root->right) return root->val;
+    //     queue<TreeNode*>q;  
+    //     q.push(root);
+    //     int sum=0;
+    //     while(!q.empty()){
+    //       int size=q.size();
+    //       sum=0;
+    //       for(int i=0;i<size;i++){
+    //         TreeNode* node=q.front();
+    //         q.pop();
+    //         sum+=node->val;
+    //         if(node->left) q.push(node->left);
+    //         if(node->right) q.push(node->right);
+    //       }
+    //     } 
+    //      return sum;
+    // }
+    
+int deepest = 0, sum = 0;
+int deepestLeavesSum(TreeNode* root, int depth = 0) {
+	if(!root) return 0;  // current node is NULL - just return.
+	if(!root -> left && !root -> right) // if current is a leaf node
+		if(depth == deepest) sum += root -> val;  // if depth is exactly equal to deepest, add to sum
+		else if(depth > deepest) sum = root -> val, deepest = depth; // found a more deeper leaf node, reset sum and update deepest
+	deepestLeavesSum(root -> left, depth + 1);   // recurse for the left, 
+	deepestLeavesSum(root -> right, depth + 1);  // and the right node of the current node
+	return sum; // finally return the sum accumulated till now which will be sum of value of deepest leaves
+}
 };
