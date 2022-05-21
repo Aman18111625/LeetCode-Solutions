3b88c9c473ebb911c2406a9557abf5f3d8cf0eb4
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-21 11:19:19 +0530

    Time: 4 ms (76.09%), Space: 9.5 MB (79.47%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -19,30 +19,50 @@ public:
   //so simply we'll check in which row our target lies and then we'll simply apply binary search in that row
   //Time-Complexity=>O(nlogm)
   //Space-Complexity:=>O(1)
-     bool binarySearch(vector<int>&arr, int target) {
-        int low = 0, high = arr.size();
-        while (low <= high){
-            int mid = (low + high) / 2;
-            if (arr[mid] == target) return true;//found
-            else if (arr[mid] < target)//not exist in left search space
-                low = mid + 1;
-            else//not exist in right search space
-                high = mid - 1;
-        }
-       //not found
-        return false;
-    }
+//      bool binarySearch(vector<int>&arr, int target) {
+//         int low = 0, high = arr.size();
+//         while (low <= high){
+//             int mid = (low + high) / 2;
+//             if (arr[mid] == target) return true;//found
+//             else if (arr[mid] < target)//not exist in left search space
+//                 low = mid + 1;
+//             else//not exist in right search space
+//                 high = mid - 1;
+//         }
+//        //not found
+//         return false;
+//     }
     
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
-        //base case
-        if(mat.empty()) return false;
-        int row = 0;
-        int n=mat.size(),m=mat[0].size();
-        while (row <n  && (mat[row].back() < target))
-            row++;
+//     bool searchMatrix(vector<vector<int>>& mat, int target) {
+//         //base case
+//         if(mat.empty()) return false;
+//         int row = 0;
+//         int n=mat.size(),m=mat[0].size();
+//         while (row <n  && (mat[row].back() < target))
+//             row++;
         
-        if (row >= n) return false;
+//         if (row >= n) return false;
         
-        return binarySearch(mat[row], target);
+//         return binarySearch(mat[row], target);
+//     }
+     //Another way using Binary Search
+       bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int lo = 0;
+        if(!matrix.size()) return false;
+        int hi = (matrix.size() * matrix[0].size()) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] == target) {
+                return true;
+            }
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
     }
 };
