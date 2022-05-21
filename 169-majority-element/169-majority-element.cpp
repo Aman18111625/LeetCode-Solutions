37a97fcd11781010cbaaf6ff8c319e0659cc40d3
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-21 12:02:53 +0530

    Time: 32 ms (28.06%), Space: 19.6 MB (14.32%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-  /*
+  
   //Time-Complexity:=>O(N)
   //Space-Complexity:=>O(N)
     int majorityElement(vector<int>& nums) {
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
         for(auto &num: nums) mpp[num]++;
         int n=nums.size()/2;
         for(auto &it : mpp){
           if(it.second>n) return it.first;
         }
       return -1;
-    }*/
+    }
   //Time-COmplexity:=>O(N)
   //Space-Complexity:=>O(1)
-   int majorityElement(vector<int>& nums) {
-        int count=0,candidates=0;
-        for(auto &num:nums)
-        {
-         if(count==0) candidates=num;
-         if(num==candidates) count++;
-         else count--;
-        }
-        return candidates;
-    }
+   // int majorityElement(vector<int>& nums) {
+   //      int count=0,candidates=0;
+   //      for(auto &num:nums)
+   //      {
+   //       if(count==0) candidates=num;
+   //       if(num==candidates) count++;
+   //       else count--;
+   //      }
+   //      return candidates;
+   //  }
 };
