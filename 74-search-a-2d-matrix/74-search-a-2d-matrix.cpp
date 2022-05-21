dbd07b0ae7fe15dc199ad00aaf4798792a0836bc
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-21 11:05:40 +0530

    Time: 4 ms (76.09%), Space: 9.5 MB (79.47%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
-     bool binarySearch(vector<int>& vec, int target) {
-        int l = 0, r = vec.size();
-        while (l <= r)
-        {
-            int mid = (l + r) / 2;
-            if (vec[mid] == target)
-                return true;
-            else if (vec[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
-        }
-        return false;
-    }
+  //Brute-Force Approach
+  //Time-complexity:=>O(M*N)
+  //Space-Complexity:=>O(1)
+  //Simply traverse in whole matrix and search for target value,if found->true else false
+   bool searchMatrix(vector<vector<int>>&mat, int target){
+     int n=mat.size(),m=mat[0].size();
+     for(int i=0;i<n;i++){
+       for(int j=0;j<m;j++){
+         if(mat[i][j]==target) return true;
+       }
+     }
+     return false;
+   }
+//      bool binarySearch(vector<int>& vec, int target) {
+//         int l = 0, r = vec.size();
+//         while (l <= r)
+//         {
+//             int mid = (l + r) / 2;
+//             if (vec[mid] == target)
+//                 return true;
+//             else if (vec[mid] < target)
+//                 l = mid + 1;
+//             else
+//                 r = mid - 1;
+//         }
+//         return false;
+//     }
     
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if ((matrix.size() == 0) || (matrix[0].size() == 0)) return false;
-        int row = 0;
-        while ((row < matrix.size()) && (matrix[row].back() < target))
-            row++;
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         if ((matrix.size() == 0) || (matrix[0].size() == 0)) return false;
+//         int row = 0;
+//         while ((row < matrix.size()) && (matrix[row].back() < target))
+//             row++;
         
-        if (row >= matrix.size()) return false;
+//         if (row >= matrix.size()) return false;
         
-        return binarySearch(matrix[row], target);
-    }
+//         return binarySearch(matrix[row], target);
+//     }
 };
