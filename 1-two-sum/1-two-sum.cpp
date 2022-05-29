7c4d64f1f32cd7b4135700309f7275f43f08e913
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-29 11:38:07 +0530

    Time: 15 ms (72.94%), Space: 10.9 MB (29.68%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-          for(int j=0;j<i;j++){
-            if(nums[i]+nums[j]==target) return {i,j};
-          }
-        }
-        return ans;
+  //Brute-Force Solution->check for every pairs if sum of these two equal to target then simply return their respective indices
+  //TC:=>O(N^2)
+  //SC:=>O(1)
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     int n=nums.size();
+    //     vector<int>ans;
+    //     for(int i=0;i<n;i++){
+    //       for(int j=0;j<i;j++){
+    //         if(nums[i]+nums[j]==target) return {i,j};
+    //       }
+    //     }
+    //     return ans;
+    // }
+  //Better Solution->Using Hashmap
+  //TC:=>O(N)
+  //SC:=>O(N)
+  vector<int> twoSum(vector<int>& nums, int target){
+    vector<int>ans;
+    unordered_map<int,int>mpp;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+      int rest=target-nums[i];
+      if(mpp.find(rest)!=mpp.end()) return {i,mpp[rest]};
+      mpp[nums[i]]=i;
     }
+    return ans;
+  }
 };
