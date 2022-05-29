0a028e2ce8e8ad5fe9262fd23cb383ea6ce1120b
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-29 11:46:16 +0530

    Time: 14 ms (75.35%), Space: 10.5 MB (58.36%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -16,15 +16,45 @@ public:
   //Better Solution->Using Hashmap
   //TC:=>O(N)
   //SC:=>O(N)
-  vector<int> twoSum(vector<int>& nums, int target){
-    vector<int>ans;
-    unordered_map<int,int>mpp;
-    int n=nums.size();
-    for(int i=0;i<n;i++){
-      int rest=target-nums[i];
-      if(mpp.find(rest)!=mpp.end()) return {i,mpp[rest]};
-      mpp[nums[i]]=i;
-    }
-    return ans;
-  }
+  // vector<int> twoSum(vector<int>& nums, int target){
+  //   vector<int>ans;
+  //   unordered_map<int,int>mpp;
+  //   int n=nums.size();
+  //   for(int i=0;i<n;i++){
+  //     int rest=target-nums[i];
+  //     if(mpp.find(rest)!=mpp.end()) return {i,mpp[rest]};
+  //     mpp[nums[i]]=i;
+  //   }
+  //   return ans;
+  // }
+  //Better Approach->Using Two Pointer
+  //TC:->O(NLOGN)
+  //SC:->O(N)
+  vector<int> twoSum(vector<int>& nums, int target) {
+    	vector<int> res,store=nums;
+    	sort(store.begin(), store.end());
+    	int left=0,right=nums.size()-1;
+    	int n1,n2;
+
+    	while(left<right){
+        	if(store[left]+store[right]==target){
+            	n1 = store[left];
+            	n2 = store[right];
+            	break;
+        	}
+        	else if(store[left]+store[right]>target)
+            	    right--;
+        	else
+            	    left++;
+    	}
+
+    	for(int i=0;i<nums.size();++i){
+        	if(nums[i]==n1)
+            	    res.emplace_back(i);
+        	else if(nums[i]==n2)
+            	    res.emplace_back(i);
+    	}
+
+    	    return res;
+	}
 };
