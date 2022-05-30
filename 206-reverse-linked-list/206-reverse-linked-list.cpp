99ff8c0449f8fc98ba10f3aa968432203f40815e
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-30 11:39:41 +0530

    Time: 13 ms (19.25%), Space: 8.5 MB (20.62%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -52,15 +52,23 @@ public:
   //Optimal Approach-Recursive
   //TC:=>O(N)
   //SC:=>O(N) (stack-Space)
+   // ListNode* reverseList(ListNode* head){
+   //   if(!head || !head->next) return head;
+   //   ListNode* newHead=NULL;
+   //   while(head){
+   //     ListNode* nex=head->next;
+   //     head->next=newHead;
+   //     newHead=head;
+   //     head=nex;
+   //   }
+   //   return newHead;
+   // }
+  //Recursive
    ListNode* reverseList(ListNode* head){
-     if(!head || !head->next) return head;
-     ListNode* newHead=NULL;
-     while(head){
-       ListNode* nex=head->next;
-       head->next=newHead;
-       newHead=head;
-       head=nex;
-     }
-     return newHead;
+        if (!head|| !head->next) return head;
+        ListNode* nnode = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return nnode;
    }
 };
