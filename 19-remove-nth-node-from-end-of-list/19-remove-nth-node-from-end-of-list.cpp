83b7556a4d7b628818895b3d7885416847241149
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-30 14:25:47 +0530

    Time: 3 ms (88.75%), Space: 10.8 MB (34.11%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,21 +10,39 @@
  */
 class Solution {
 public:
-  //Brute
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head) return head;
-        if(n==1 && !head->next) return NULL;
-        int len=0;
-        ListNode* curr=head;
-        while(curr){
-          len++;
-          curr=curr->next;
-        }
-        if(len==n) return head->next;
-        curr=head;
-        for(int i=0;i<(len-n-1);i++) curr=curr->next;
-        curr->next=curr->next->next;
-        return head;
-    }
+  //Brute-Force Approach
+  //TC:=>O(2N)
+  //SC:=>O(1)
+    // ListNode* removeNthFromEnd(ListNode* head, int n) {
+    //     if(!head) return head;
+    //     if(n==1 && !head->next) return NULL;
+    //     int len=0;
+    //     ListNode* curr=head;
+    //     while(curr){
+    //       len++;
+    //       curr=curr->next;
+    //     }
+    //     if(len==n) return head->next;
+    //     curr=head;
+    //     for(int i=0;i<(len-n-1);i++) curr=curr->next;
+    //     curr->next=curr->next->next;
+    //     return head;
+    // }
+  
+  //Efficient Approach -> Using Two Pointer
+  //TC:=>O(N)
+  //SC:=>O(1)
+   ListNode* removeNthFromEnd(ListNode* head, int n){
+       ListNode * start = new ListNode();
+        start -> next = head;
+       ListNode* fast=start,*slow=start;
+       for(int i=1;i<=n;i++) fast=fast->next;
+       while(fast->next){
+         slow=slow->next;
+         fast=fast->next;
+       }
+       slow->next=slow->next->next;
+       return start->next;
+   }
 };
 
