ff09ab7bb25d023db5d346688c053241f244eaf6
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-30 15:06:04 +0530

    Time: 13 ms (36.08%), Space: 14.9 MB (14.99%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,36 +10,59 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-           if(!list1 && !list2) return NULL;
-           if(!list1) return list2;
-           if(!list2) return list1;
-           ListNode* dummy = new ListNode();
-           ListNode* temp=dummy;
-           while(list1 && list2){
-              if(list1->val<=list2->val){
-                temp->next=new ListNode(list1->val);
-                list1=list1->next;
-              }else {
-                temp->next=new ListNode(list2->val);
-                list2=list2->next;
-              }
-             temp=temp->next;
-           }
+  //Brute Force Approach
+  //TC:=>O(N+M)
+  //SC:=>O(N+M)
+  //we're create new Linked List 
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//            if(!list1 && !list2) return NULL;
+//            if(!list1) return list2;
+//            if(!list2) return list1;
+//            ListNode* dummy = new ListNode();
+//            ListNode* temp=dummy;
+//            while(list1 && list2){
+//               if(list1->val<=list2->val){
+//                 temp->next=new ListNode(list1->val);
+//                 list1=list1->next;
+//               }else {
+//                 temp->next=new ListNode(list2->val);
+//                 list2=list2->next;
+//               }
+//              temp=temp->next;
+//            }
            
-          while(list1){
-            temp->next=new ListNode(list1->val);
-            temp=temp->next;
-            list1=list1->next;
-          }
+//           while(list1){
+//             temp->next=new ListNode(list1->val);
+//             temp=temp->next;
+//             list1=list1->next;
+//           }
          
-         while(list2){
-           temp->next= new ListNode(list2->val);
-           temp=temp->next;
-           list2=list2->next;
+//          while(list2){
+//            temp->next= new ListNode(list2->val);
+//            temp=temp->next;
+//            list2=list2->next;
+//          }
+//         return dummy->next;
+//     }
+  
+  //Efficient Approach-> Without Using Extra Space
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+       if(!list1 && !list2) return NULL;
+       if(!list2) return list1;
+       if(!list1) return list2;
+       if(list1->val>list2->val) swap(list1,list2);
+       ListNode* res=list1;
+       while(list1 && list2){
+         ListNode* temp=NULL;
+         while(list1 && list1->val<=list2->val){
+           temp=list1;
+           list1=list1->next;
          }
-        return dummy->next;
-    }
+         temp->next=list2;
+         swap(list1,list2);
+       }
+      return res;
+  }
 };
 
 
