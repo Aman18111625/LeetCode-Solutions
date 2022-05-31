367e40c03ee85c82d8a1ad8ee5537576fed3f43e
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-31 11:41:45 +0530

    Time: 65 ms (45.10%), Space: 14.6 MB (73.35%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -27,17 +27,51 @@ public:
     //Better Approach->Using Hashing->store node address of list1 in set then search of node of list2 .If find then we got our answer else return NULL at the end,
     //TC:=>O(M+N)
     //SC:=>O(M)
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){ 
-      unordered_set<ListNode*>st;
-      while(headA){
-        st.insert(headA);
-        headA=headA->next;
-      } 
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){ 
+//       unordered_set<ListNode*>st;
+//       while(headA){
+//         st.insert(headA);
+//         headA=headA->next;
+//       } 
       
+//       while(headB){
+//         if(st.count(headB)) return headB;
+//         headB=headB->next;
+//       }
+//       return NULL;
+//     }
+  
+//Optimal Approach ->Using Difference of Length.
+//Find length of both the lists.
+// Find the positive difference of these lengths.
+// Move the dummy pointer of the larger list by difference achieved. This makes our search length reduced to the smaller list length.
+// Move both pointers, each pointing two lists, ahead simultaneously if both do not collide.
+  //TC:=>O(N+M)
+  //SC:=>O(1)
+    int getDifference(ListNode* headA,ListNode* headB){
+      int len1=0,len2=0;
+      while(headA){
+        len1++;
+        headA=headA->next;
+      }
       while(headB){
-        if(st.count(headB)) return headB;
+        len2++;
         headB=headB->next;
       }
-      return NULL;
+      return len1-len2;
     }
+   ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+          int diff=getDifference(headA,headB);
+          if(diff<0) 
+            while(diff++!=0) headB=headB->next;
+          else
+            while(diff--!=0) headA=headA->next;
+          while(headA && headB){
+            if(headA==headB) return headA;
+            headA=headA->next;
+            headB=headB->next;
+          }
+         return NULL;
+   }
+  
 };
