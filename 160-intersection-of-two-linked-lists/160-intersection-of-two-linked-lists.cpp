8b015dd3cd1a20deeab10d6a221e5e76bcda23b8
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-31 11:33:01 +0530

    Time: 91 ms (19.92%), Space: 17.2 MB (15.26%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -12,15 +12,32 @@ public:
   //TC:=>O(M*N)
   //SC:=>O(1)
   //Approach-> As we know intersection means common attribute. To find common attribute in 2 Linked-List, Here we'll choose 2nd LL and check for it's node present in 1st LL or not.If present then we got our answer otherwise repeat this steps until head2!=NULL. 
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-       while(headB){
-         ListNode* temp=headA;
-         while(temp){
-           if(temp==headB) return temp;
-           temp=temp->next;
-         }
-         headB=headB->next;
-       }   
+    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    //    while(headB){
+    //      ListNode* temp=headA;
+    //      while(temp){
+    //        if(temp==headB) return temp;
+    //        temp=temp->next;
+    //      }
+    //      headB=headB->next;
+    //    }   
+    //   return NULL;
+    // }
+  
+    //Better Approach->Using Hashing->store node address of list1 in set then search of node of list2 .If find then we got our answer else return NULL at the end,
+    //TC:=>O(M+N)
+    //SC:=>O(M)
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){ 
+      unordered_set<ListNode*>st;
+      while(headA){
+        st.insert(headA);
+        headA=headA->next;
+      } 
+      
+      while(headB){
+        if(st.count(headB)) return headB;
+        headB=headB->next;
+      }
       return NULL;
     }
 };
