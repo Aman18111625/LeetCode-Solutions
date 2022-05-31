8098c9e64f06ce1a30b014c4696efa2037e8f1fd
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-05-31 12:22:17 +0530

    Time: 392 ms (20.67%), Space: 114.1 MB (85.14%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -14,14 +14,63 @@ public:
    //TC:=>O(N)
   //SC:=>O(N)
   //Simply store values in the array and check if arr[i] is equal to arr[n-i-1] or not .If not then it's not palindrome else it's palindrome
+    // bool isPalindrome(ListNode* head) {
+    //     vector<int> arr;
+    //     while(head) {
+    //      arr.push_back(head->val);
+    //      head = head->next;
+    //    }
+    //    for(int i=0;i<arr.size()/2;i++) 
+    //      if(arr[i] != arr[arr.size()-i-1]) return false;
+    //    return true;
+    // }
+  
+  //Optimized Approach
+  //TC:=>O(N)
+  //SC:=>O(1)
+  //step1 ->find middle node
+  //step2 -> now reverse the whole list after  middle node
+  //step3 -> now compare values of node before middle node and after middle node.
+  
+  //finding mid node
+    ListNode* findMid(ListNode* head){
+        ListNode* slow=head,*fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next;
+            if(fast) fast=fast->next;
+        }
+        return slow;
+    }
+    
+  //reversing the list
+    ListNode* reverseList(ListNode* head){
+        ListNode* curr=head;
+        ListNode* temp=NULL;
+        ListNode* prev=NULL;
+        while(curr){
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    
+    bool compareList(ListNode* head1, ListNode* head2){
+        while(head1 && head2){
+            if(head1->val != head2->val) return false;
+            head1=head1->next;
+            head2=head2->next;
+        }
+        return true;
+    }
+    
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        while(head) {
-         arr.push_back(head->val);
-         head = head->next;
-       }
-       for(int i=0;i<arr.size()/2;i++) 
-         if(arr[i] != arr[arr.size()-i-1]) return false;
-       return true;
+       if(!head or !head->next) return true;
+        ListNode* curr=head;
+        ListNode* mid=findMid(curr);
+        ListNode* head1=reverseList(mid);
+        return compareList(head,head1);
     }
 };
