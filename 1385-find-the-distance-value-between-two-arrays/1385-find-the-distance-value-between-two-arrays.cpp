acbcc76af953f2bd7a958f40451b49e3c207733d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-01 10:37:02 +0530

    Time: 19 ms (40.14%), Space: 12.9 MB (95.46%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,16 +1,42 @@
 class Solution {
 public:
-    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        int cnt=0;
-        for(auto num1: arr1){
-           bool flag=true;
-          for(auto num2: arr2){
-            if(abs(num1-num2)<=d){
-              flag=false;
-            }
-          }
-          if(flag) cnt++;
-        }
-        return cnt;
-    }
+  //In the 1st attempt I got WA because i mis-read the question 
+  //Brute-Force Appraoch
+  //TC:=>O(M*N)
+  //SC:=>O(1)
+    // int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+    //     int cnt=0;
+    //     for(auto num1: arr1){
+    //        bool flag=true;
+    //       for(auto num2: arr2){
+    //         if(abs(num1-num2)<=d){
+    //           flag=false;
+    //         }
+    //       }
+    //       if(flag) cnt++;
+    //     }
+    //     return cnt;
+    // }
+  
+    //Optima
+     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+       int cnt=0;
+       sort(begin(arr2),end(arr2));
+       for(auto num1 : arr1){
+         int low=0,high=arr2.size()-1;
+         bool flag=true;
+         while(low<=high){
+           int mid=low+(high-low)/2;
+           if(abs(num1-arr2[mid])<=d){
+             flag=false; break;
+           }else if(arr2[mid]>num1){
+             high=mid-1;
+           }else {
+             low=mid+1;
+           }
+         }
+         if(flag) cnt++;
+       }
+       return cnt;
+     }
 };
