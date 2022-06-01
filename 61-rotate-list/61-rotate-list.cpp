0414859da6dd9e7621d214e7660174768f6bf240
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-01 11:27:05 +0530

    Time: 24 ms (5.39%), Space: 11.7 MB (56.64%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,6 +10,26 @@
  */
 class Solution {
 public:
+  //Brute-Force Approach
+  //TC:=>O(N*K)
+  //SC:=>O(1)
+  //moving last k node to first of the Linked-List
+  // ListNode* rotateRight(ListNode* head,int k) {
+  //   if(!head || !head->next) return head;
+  //   for(int i=0;i<k;i++) {
+  //       ListNode* temp = head;
+  //       while(temp->next->next) temp = temp->next;
+  //       ListNode* end = temp->next;
+  //       temp->next = NULL;
+  //       end->next = head;
+  //       head = end;
+  //   }
+  //   return head;
+  //  }
+  
+    //Optimal Approach
+    //TC:=>O(N)
+    //SC:=>O(1)
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head || !head->next || k==0) return head;
         int n=1;
