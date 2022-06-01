92416c5adcb55dfef2b10eed86ed8091a4473e19
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-01 10:43:27 +0530

    Time: 4 ms (21.96%), Space: 5.9 MB (66.10%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
-    bool isPerfectSquare(int num) {
-        int temp=sqrt(num);
-        return temp*temp==num;
+     //Brute-Force ->Using inbuilt sqrt() function
+    //TC:=>(sqrt(num))
+    //SC:=>O(1)
+    // bool isPerfectSquare(int num) {
+    //     int temp=sqrt(num);
+    //     return temp*temp==num;
+    // }
+  //Optimal Approach->Using Binary Search
+  //why Binary Search->As we have a search range so we can simply apply BS and check whether given num is perfect square or not.
+  bool isPerfectSquare(int num) {
+        if (num == 1) return true;
+        long long lo = 1;
+        long long hi = num / 2;
+        
+        while(lo <= hi) {
+            long long mid = (lo + hi) / 2;
+            long long sq = mid * mid;
+            if (sq == (long long )num) return true;
+            else if (sq < num) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return false;
     }
 };
