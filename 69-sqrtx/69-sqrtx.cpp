40c37885ec78498ecd5e1b5b61a3c33bfb6db80a
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-02 10:36:23 +0530

    Time: 102 ms (5.28%), Space: 5.9 MB (26.27%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      return sqrt(x);   
+     // return sqrt(x);
+        if(x<2) return x;
+        auto i=1;
+        while(x/i>i)
+        {
+          i++;
+        }
+        return x/i;
     }
 };
