5e2a9611ba064f2caffac7177ea2e0cda8d6e83c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-02 10:45:09 +0530

    Time: 22 ms (49.52%), Space: 16 MB (26.27%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(auto &ch : letters){
-          if(ch>target) return ch;
-        }
-        return letters[0];
-    }
+        //Brute-Force Approach->TC:=>O(N),SC:=>O(1)
+        // for(auto &ch : letters){
+        //   if(ch>target) return ch;
+        // }
+        // return letters[0];
+      //Optimized Using Binary Search ->TC:=>O(logN),SC:=>O(1)
+      int mid, low = 0, high = letters.size() - 1;
+	      	while(low <= high){
+			     mid = (low + high) / 2;
+			     if(letters[mid] <= target){
+			     	low = mid + 1;
+			     }
+			     else{
+				    high = mid-1;
+			     }
+	  	 }
+		    if(letters[low] > target){
+			    return letters[low];
+		    }
+		   return letters[0];
+     }
 };
