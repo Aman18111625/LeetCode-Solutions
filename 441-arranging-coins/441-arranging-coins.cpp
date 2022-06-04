b7458eecff477a251e8e63658bbb443e74fb40ba
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-04 10:25:21 +0530

    Time: 6 ms (51.29%), Space: 5.9 MB (72.35%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
-    int arrangeCoins(int n) {
-        int i=1;
-        while(n>=i){
-          n-=i;
-          i++;
+    //Brute-Force Approach->Simple run a loop till n>=i and every time do n-=i && i++ 
+    //TC:=>O(sqrt(N)) && SC:=>O(1)
+    // int arrangeCoins(int n) {
+    //     int i=1;
+    //     while(n>=i){
+    //       n-=i;
+    //       i++;
+    //     }
+    //     return  i-1;
+    // }
+    //Optimal Approach->Using Binary Search
+    //TC:=>o(logN) && SC:=>O(1)
+    //we know that sum of  number from 1 to n = n*(n+1)/2;
+    //here min sum can be =0 && maxi can be =n*(n+1)/2 -> this is our search space
+    int arrangeCoins(int n){
+        long lo = 0, hi = n, mid, tmp;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            tmp = mid * (mid + 1) / 2;
+            if (tmp == n) return mid;
+            if (tmp <= n) lo = mid + 1;
+            else hi = mid - 1;
         }
-        return  i-1;
+        return lo-1;
     }
 };
