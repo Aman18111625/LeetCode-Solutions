584daf6ca0c6fda8a03539ccb4635ab21687e2e5
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-04 10:28:41 +0530

    Time: 13 ms (15.98%), Space: 9.5 MB (67.32%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    /*
+    /* 
+    //Brute-Force Approach
+    //TC:=>O(N) && SC:=>O(N)
+    //insert all the number into set and find kth missing number.
     int findKthPositive(vector<int>& arr, int k) {
         int no=1,cnt=0;
         unordered_set<int>s;
@@ -15,19 +18,21 @@ public:
             }
         return no-1;
     }*/
+   // Optimal Approach->Using binary Search
+  //TC:=>O(logN) && SC:=>O(1)
     int findKthPositive(vector<int>& arr, int k){
-        int i=0,j=arr.size()-1,mid,ans;
-        while(i<=j)
+        int low=0,high=arr.size()-1,mid,ans;
+        while(low<=high)
         {
-            int mid=(i+j)>>1;
-            int missed=arr[mid]-mid-1; //tells us the missed number till mid                 
+            int mid=(low+high)>>1;
+            int missed=arr[mid]-mid-1; //tells us the missed number till mid               
             if(missed>=k)
             {
                 ans=arr[mid]-(missed-k)-1;
-                j=mid-1;
+                high=mid-1;
             }else
             {
-                i=mid+1;
+                low=mid+1;
                 ans=arr[mid]+k-missed;
             }
         }
