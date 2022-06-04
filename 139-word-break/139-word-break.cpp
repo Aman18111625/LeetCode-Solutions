6dd9a994c71f8f2cd5f59c081864770a1f5fa75c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-04 22:48:57 +0530

    Time: 12 ms (61.93%), Space: 8 MB (78.55%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -19,23 +19,52 @@ public:
 //         return canBrk(0,s,dict);    
 //     }
   //Optimal Solution->Using Dynamic Programming
+  //Recursive+Meoization
+  //TC:=>O(N^2)
+  //SC:=>O(N)+O(N)(stack-Space)
+//    bool canBrk(int start, string& s, unordered_set<string>& dict,vector<int>&dp){
+//     int n = s.size();
+//     if(start == n) return 1;
+//     if(dp[start]!=-1) return dp[start];
+//     string sub;
+//     for(int i = start; i<n; i++)
+//         if(dict.count(sub+=s[i]) && canBrk(i+1,s,dict,dp)) return dp[start]=1;
+//     return dp[start]=0;
+//   }
+
+//      bool wordBreak(string s, vector<string>& wordDict) {
+//          unordered_set<string>dict;
+//          //insert all the dictionary words into set
+//          for(string& word: wordDict) dict.insert(word);
+//          vector<int>dp(s.size(),-1);
+//          return canBrk(0,s,dict,dp);    
+//     }
+  
+  //Top-Down Appraoch
   //TC:=>O(N^2)
   //SC:=>O(N)
-   bool canBrk(int start, string& s, unordered_set<string>& dict,vector<int>&dp){
-    int n = s.size();
-    if(start == n) return 1;
-    if(dp[start]!=-1) return dp[start];
-    string sub;
-    for(int i = start; i<n; i++)
-        if(dict.count(sub+=s[i]) && canBrk(i+1,s,dict,dp)) return dp[start]=1;
-    return dp[start]=0;
-  }
-
-     bool wordBreak(string s, vector<string>& wordDict) {
-         unordered_set<string>dict;
-         //insert all the dictionary words into set
-         for(string& word: wordDict) dict.insert(word);
-         vector<int>dp(s.size(),-1);
-         return canBrk(0,s,dict,dp);    
-    }
+   bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        int dp[n+1];
+        memset(dp,0,sizeof dp);
+        dp[n]=1;
+        
+        unordered_set<string>dict; //to store all the words of dictionary into set
+        for(string& word : wordDict)
+            dict.insert(word);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+          string check;
+         for(int j=i;j<n;j++)
+         {
+             check.push_back(s[j]);
+             if(dict.find(check)!=dict.end())
+             {
+               if(dp[j+1])dp[i]=1;
+             }
+         }  
+        }
+        return dp[0];
+   }  
 };
