3dbef867940159e946c69e2006df28966e292d4f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-05 11:16:46 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (56.47%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -3,17 +3,19 @@ class Solution {
   //Brute-Force Appraoch->simply run nested loop and check for every number ,how many are greater or equal to that number
   //TC:=>O(N^2)
   //SC:=>O(1)
-    int specialArray(vector<int>& nums) {
-        for(int i=1;i<1000;i++){
-            int cnt=0;
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]>=i) cnt++;
-            }
-            if(cnt==i) return i;
-        }
-        return -1;
-    }
-  
+    // int specialArray(vector<int>& nums) {
+    //     for(int i=1;i<1000;i++){
+    //         int cnt=0;
+    //         for(int j=0;j<nums.size();j++){
+    //             if(nums[j]>=i) cnt++;
+    //         }
+    //         if(cnt==i) return i;
+    //     }
+    //     return -1;
+    // }
+  //Better Approach->Apply binary search ->Search Space-> 0 to maximum 
+  //TC:=>O(NLOGN)
+  //SC:=>O(1)
 //    int count(vector<int>&nums,int mid){
 //      int cnt=0;
 //      for(auto num: nums){
@@ -21,7 +23,7 @@ class Solution {
 //      }
 //      return cnt;
 //    }
-// public:
+
 //     int specialArray(vector<int>& nums) {
 //         int maxi=INT_MIN;
 //         for(auto num : nums) maxi=max(maxi,num);
@@ -35,4 +37,18 @@ class Solution {
 //         }
 //         return -1;
 //     }
+    //Optimal Solution:->Using Count Sort
+    //TC:=>O(N)
+    //SC:=>O(N)
+     int specialArray(vector<int>& nums) {
+        int freq[1001]={0};
+        for(int num: nums) freq[num]++;
+        int total=nums.size();
+        for(int i=0;i<1001;i++){
+            if(i==total) 
+                return i;
+            total-=freq[i];
+        }
+        return -1;
+    }
 };
