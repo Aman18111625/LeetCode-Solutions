d4223449770ae76e2b965b3a8b9ead3db7cc7254
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-05 11:13:53 +0530

    Time: 11 ms (6.74%), Space: 8.2 MB (99.47%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,23 +1,38 @@
 class Solution {
-   int count(vector<int>&nums,int mid){
-     int cnt=0;
-     for(auto num: nums){
-       if(num>=mid) cnt++;
-     }
-     return cnt;
-   }
-public:
+  public:
+  //Brute-Force Appraoch->simply run nested loop and check for every number ,how many are greater or equal to that number
+  //TC:=>O(N^2)
+  //SC:=>O(1)
     int specialArray(vector<int>& nums) {
-        int maxi=INT_MIN;
-        for(auto num : nums) maxi=max(maxi,num);
-        int low=0,high=maxi;
-        while(low<=high){
-          int mid=(low+high)/2;
-          int cnt=count(nums,mid);
-          if(cnt==mid) return mid;
-          else if(cnt>mid) low=mid+1;
-          else high=mid-1;
+        for(int i=1;i<1000;i++){
+            int cnt=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]>=i) cnt++;
+            }
+            if(cnt==i) return i;
         }
         return -1;
     }
+  
+//    int count(vector<int>&nums,int mid){
+//      int cnt=0;
+//      for(auto num: nums){
+//        if(num>=mid) cnt++;
+//      }
+//      return cnt;
+//    }
+// public:
+//     int specialArray(vector<int>& nums) {
+//         int maxi=INT_MIN;
+//         for(auto num : nums) maxi=max(maxi,num);
+//         int low=0,high=maxi;
+//         while(low<=high){
+//           int mid=(low+high)/2;
+//           int cnt=count(nums,mid);
+//           if(cnt==mid) return mid;
+//           else if(cnt>mid) low=mid+1;
+//           else high=mid-1;
+//         }
+//         return -1;
+//     }
 };
