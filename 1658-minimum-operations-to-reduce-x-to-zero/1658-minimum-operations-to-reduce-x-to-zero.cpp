252aad56ed07d927481986305b57d5ff7113df58
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-11 10:58:14 +0530

    Time: 247 ms (66.54%), Space: 98.5 MB (90.95%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+//Brute-Force Approach:=> Hash map, prefix sum, two sum
+//Idea:  create hash map satisfy mapLeft[prefix sum] = i, i is last index in prefix sum
+//brute force from last index in array to first index and create suffix sum, find i satisfy mapLeft[x - suffix sum] > 0 
+//that's mean prefix sum + suffix sum = x,  and min operation = min (min operation, size(prefix sum) + size(suffix sum) );
+//TC: O(n)
+//SC: O(n)
+// 	int minOperations(vector<int>& nums, int x) {
+// 		unordered_map<int, int> mapLeft;
+// 		int n = nums.size();
+// 		int ans = INT_MAX;
+
+// 		for(int i = 0, prefixSum = 0; i < n; i++) {
+// 			prefixSum += nums[i];
+// 			mapLeft[prefixSum] = i + 1;
+// 			if (prefixSum == x)
+// 				ans = min(ans, i + 1);
+// 		}
+
+// 		for(int i = n - 1, suffixSum = 0; i >= 0; i--) {
+// 			suffixSum += nums[i];
+// 			if(suffixSum == x)
+// 				ans = min(ans, n - i);
+// 			if(mapLeft[x - suffixSum] > 0 && i + 1 > mapLeft[x - suffixSum]) // if i + 1 <= mapLeft => both prefix sum and suffix sum have same elements
+// 				ans = min(ans, n - i + mapLeft[x - suffixSum]); // n - i is size suffix sum, mapLeft[x - suffixSum] is size prefix sum
+// 		}
+// 		return ans == INT_MAX ? -1 : ans;
+// 	}
+  
+  
+  //Optimal Approach
   //so according to the question ->we can remove leftmost or rightmost element and we have to remove such that->
   //1.sum of removed elements is equal to X
   //2.remove in minimum number of operations
