45f1c049fd3a7682f21cdc27ed9d06085d3c693c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-12 10:36:42 +0530

    Time: 265 ms (90.77%), Space: 89.2 MB (97.81%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n=nums.size();
-        unordered_set<int>st;//to keep track of duplicate value
+        int freq[10001]={false};//to keep track of duplicate value
         int i=0,j=0,curr=0;//i ->beginning of subarray & j->end of subarray
         int ans=INT_MIN;//to store maximum result
         while(j<n){
-          while(st.find(nums[j])!=st.end()){//delete from current sub-array till there's no duplicate of nums[j]
+          while(freq[nums[j]]){//delete from current sub-array till there's no duplicate of nums[j]
              curr-=nums[i];
-             st.erase(nums[i]);
+             freq[nums[i]]=false;
              i++;
           }
           curr+=nums[j];//include in our subarray
-          st.insert(nums[j]);//insert into set
+          freq[nums[j]]=true;//mark as visited
           ans=max(ans,curr);//update ans with maximum till now
           j++;
         }
