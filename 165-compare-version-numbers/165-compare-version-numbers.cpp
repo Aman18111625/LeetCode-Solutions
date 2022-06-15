227b9337372c52bf704eb21920a6638832f6667f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-15 16:23:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (93.94%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -3,20 +3,17 @@ public:
     int compareVersion(string version1, string version2) {
         int n=version1.size(),m=version2.size();
         int i=0,j=0;
-        while(i<n || j<m)
-        {
+        while(i<n or j<m){
           int num1=0,num2=0;
-          while(i<n && version1[i]!='.')
-          {
-              num1=num1*10+(version1[i++]-'0');
+          while(i<n && version1[i]!='.'){
+            num1=num1*10+(version1[i++]-'0');
           }
-          while(j<m && version2[j]!='.')
-          {
-              num2=num2*10+(version2[j++]-'0');  
+          while(j<m && version2[j]!='.'){
+            num2=num2*10+(version2[j++]-'0');  
           }
           if(num1>num2) return 1;
           if(num2>num1) return -1;
-         i++,j++;
+          i++,j++;
         }
         return 0;
     }
