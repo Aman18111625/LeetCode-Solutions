1f5aac059e6fa5361a521f42a68133f6869d70e7
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-16 12:52:59 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -100,24 +100,23 @@ class Solution {
         vector<int> ans; 
         if(!root) return ans; 
         map<int,int> mpp; 
-        queue<pair<Node*, int>> q; 
+        queue<pair<Node*, int>> q; //key->node & val->height
         q.push({root, 0}); 
         while(!q.empty()) {
-            auto it = q.front(); 
-            q.pop(); 
-            Node* node = it.first; 
-            int line = it.second; 
-            mpp[line] = node->data; 
-            
-            if(node->left != NULL) {
+            Node* node = q.front().first; 
+            int line = q.front().second; 
+            q.pop();
+            mpp[line] = node->data; //update because we have to take last updated data
+            //left then line=line-1
+            if(node->left) {
                 q.push({node->left, line-1}); 
             }
-            if(node->right != NULL) {
+            //right then line=line+1
+            if(node->right) {
                 q.push({node->right, line + 1}); 
             }
             
         }
-        
         for(auto it : mpp) {
             ans.push_back(it.second); 
         }
