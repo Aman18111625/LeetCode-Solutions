26870473bdc5b5d41f08fee03f29f8d34e3915ce
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-16 12:11:02 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (77.18%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -14,18 +14,34 @@ public:
    //Iterative Approach
    //TC:=>O(N) 
    //SC:=>O(N)
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(!root) return ans;
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-          TreeNode* node=st.top(); 
-          ans.push_back(node->val);
-          st.pop();
-          if(node->right) st.push(node->right);
-          if(node->left) st.push(node->left);
-        }
-        return ans;
-    }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int>ans;
+    //     if(!root) return ans;
+    //     stack<TreeNode*>st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //       TreeNode* node=st.top(); 
+    //       ans.push_back(node->val);
+    //       st.pop();
+    //       if(node->right) st.push(node->right);
+    //       if(node->left) st.push(node->left);
+    //     }
+    //     return ans;
+    // }
+   //Recursive Approach
+   //TC:=>O(N)
+   //SC:=>O(N)
+    void preOrder(TreeNode* root,vector<int>&pre){
+      if(!root) return ;
+      pre.push_back(root->val);
+      preOrder(root->left,pre);
+      preOrder(root->right,pre);
+    } 
+  
+   vector<int> preorderTraversal(TreeNode* root){
+      vector<int>pre;
+      if(!root) return pre;
+      preOrder(root,pre);
+      return pre;
+  }
 };
