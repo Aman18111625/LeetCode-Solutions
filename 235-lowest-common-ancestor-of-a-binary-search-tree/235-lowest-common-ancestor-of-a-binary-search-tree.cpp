a78e4abf7b0de225b2a052e7b688b91914e79245
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-18 16:48:09 +0530

    Time: 34 ms (81.75%), Space: 23.3 MB (63.53%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,7 +14,7 @@ public:
       if(!root || root==p || root==q) return root;
       TreeNode* left=lowestCommonAncestor(root->left,p,q);
       TreeNode* right=lowestCommonAncestor(root->right,p,q);
-       if(left && right) return root;
-        return left!=NULL?left:right;
+      if(left && right) return root;
+      return left?left:right;
     }
 };
