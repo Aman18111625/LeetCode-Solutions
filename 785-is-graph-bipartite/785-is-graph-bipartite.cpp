92effe1829c939b78a46b19843546029bf1a7126
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-22 13:54:46 +0530

    Time: 31 ms (63.97%), Space: 13.6 MB (38.41%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,27 +1,50 @@
 class Solution {
-  bool checkBipartite(vector<vector<int>>&graph,int node,vector<int>&color){
-    queue<int>q;
-    q.push(node);
-    color[node]=1;
-    while(!q.empty()){
-      auto temp=q.front();q.pop();
-      for(auto &it: graph[temp]){
-       if(color[it]==-1){
-        color[it]=1-color[temp];
-        q.push(it);
-       }else if(color[it]==color[temp]) return false;
-      }
-    }
-    return true;
-  }
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-         int n=graph.size();
-         vector<int>color(n,-1);
-         for(int i=0;i<n;i++){
-           if(color[i]==-1)
-             if(!checkBipartite(graph,i,color)) return false;
-         }
+//BFS Appraoch
+//   bool checkBipartite(vector<vector<int>>&graph,int node,vector<int>&color){
+//     queue<int>q;
+//     q.push(node);
+//     color[node]=1;
+//     while(!q.empty()){
+//       auto temp=q.front();q.pop();
+//       for(auto &it: graph[temp]){
+//        if(color[it]==-1){
+//         color[it]=1-color[temp];
+//         q.push(it);
+//        }else if(color[it]==color[temp]) return false;
+//       }
+//     }
+//     return true;
+//   }
+
+//     bool isBipartite(vector<vector<int>>& graph) {
+//          int n=graph.size();
+//          vector<int>color(n,-1);
+//          for(int i=0;i<n;i++){
+//            if(color[i]==-1)
+//              if(!checkBipartite(graph,i,color)) return false;
+//          }
+//       return true;
+//     }
+  
+  bool dfs(vector<vector<int>>&graph,vector<int>&color,int node){
+      for(auto it : graph[node]){
+        if(color[it]==-1){
+           color[it]=1-color[node];
+           if(dfs(graph,color,it)==false) return false;
+        }else if(color[it]==color[node]) return false;
+      }
       return true;
-    }
+  }
+  
+  bool isBipartite(vector<vector<int>>& graph){
+      int n=graph.size();
+      vector<int>color(n,-1);
+      for(int i=0;i<n;i++){
+        if(color[i]==-1){
+          if(!dfs(graph,color,i)) return false;
+        }
+      }
+     return true;
+  }
 };
