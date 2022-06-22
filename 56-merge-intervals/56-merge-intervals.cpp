342501a4e1703b0b15ab64fea3e836dd55312ffd
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-22 17:41:39 +0530

    Time: 48 ms (68.58%), Space: 20.1 MB (26.35%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,8 +1,40 @@
 class Solution {
 public:
+     // Brute Force Solution
+    //TC:->O(N^2) 
+    //Auxiliary Space:-> O(1)
+    //   vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //     int len=intervals.size();
+    //     if(len<=1)
+    //         return intervals;
+    //     sort(intervals.begin(),intervals.end()); //Time taken by sort() is O(NlogN)
+    //     vector<vector<int>> res;
+    //     for(int i=0;i<len;i++){
+    //         int a=intervals[i][0];
+    //         int b=intervals[i][1];
+    //         // for loop inside for loop takes time of O(N^2)
+    //         for(int j=i+1;j<len;j++){
+    //             int c=intervals[j][0];
+    //             int d=intervals[j][1];
+    //             if(b>=c){ //if end of prev interval >= start of next interval
+    //                 b=max(b,d);
+    //                 // i pointer should now point to the pair pointed by j 
+    //                 // and in next iteration of j loop, j will point to the 
+    //                 // pair next to the one pointed by this i
+    //                 i=j;
+    //             }
+    //         } 
+    //         res.push_back({a,b});
+    //     }
+    //     return res;
+    // }
+  
+  //Intuition: Since we have sorted the intervals, the intervals which will be merging are bound to be adjacent. We kept on merging simultaneously as we were traversing through the array and when the element was non-overlapping we simply inserted the element in our data structure.
+  //Time-Complexity:=>O(N)
+  //Space-Complexity:=>O(N)
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>mergeIntervals;
-        if(intervals.size()==0) return mergeIntervals;
+        vector<vector<int>>mergeIntervals;//to store ans
+        if(intervals.empty()) return mergeIntervals;
         sort(intervals.begin(),intervals.end());
         vector<int>tempInterval=intervals[0];
         for(auto it:intervals){
@@ -11,7 +43,7 @@ public:
           }
           else{
            mergeIntervals.push_back(tempInterval);
-              tempInterval=it;
+           tempInterval=it;
           }
         }
         //for last interval
