bc596a56164aaa602dd3042a5fd2cf05dcad8e5c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-22 12:47:41 +0530

    Time: 25 ms (78.21%), Space: 14.3 MB (42.93%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,68 +1,64 @@
 class Solution {
   public:
-//    bool topo(int start,vector<int>adj[], int n,vector<bool>&vis, vector<bool>&s){
-//         vis[start]=true;
-//         s[start]=true;
-//         for(int it: adj[start]){
-//             if(!vis[it]){
-//               if(topo(it,adj,n,vis,s))
-//                   return true;
-//             }
-//             else if(s[it])
-//                 return true;
-//         }
-//         s[start]=false;
-//         return false;
-//     }
+  //DFS Traversal
+   bool topo(int start,vector<int>adj[], int n,vector<bool>&vis, vector<bool>&s){
+        vis[start]=true;
+        s[start]=true;
+        for(int it: adj[start]){
+            if(!vis[it]){
+              if(topo(it,adj,n,vis,s))
+                  return true;
+            }
+            else if(s[it])
+                return true;
+        }
+        s[start]=false;
+        return false;
+    }
     
     
-//     bool canFinish(int courses, vector<vector<int>>& prerequisites) 
-//     {
-//         int n =courses;
-//         vector<int>adj[n];
-//         for(int i=0;i<prerequisites.size();i++)
-//             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+    bool canFinish(int courses, vector<vector<int>>& pre){
+        int n =courses;
+        vector<int>adj[n];
+        for(auto it : pre)
+            adj[it[1]].push_back(it[0]);
         
-//         vector<bool>s(n,false),vis(n,false);
-        
-//         for(int i=0;i<n;i++)
-//         {
-//             if(topo(i,adj,n,vis,s)) //if cycle detected then return false
-//                 return false;
-//         }
-        
-//         return true;
-        
-//     }
+        vector<bool>s(n,false),vis(n,false);
+        for(int i=0;i<n;i++){
+            if(topo(i,adj,n,vis,s)) //if cycle detected then return false
+                return false;
+        }
+        return true;
+    }
     
   
- 
-    bool canFinish(int num, vector<vector<int>>&a) {
-        int n=a.size(),cnt=0;
-        vector<int> in(num,0) , adj[num +1];
-        queue<int> q;
-        for(int i=0;i<n;i++) 
-          adj[a[i][1]].push_back(a[i][0]);//storing directed graph
+//BFS Traversal using Kah
+//     bool canFinish(int num, vector<vector<int>>&a) {
+//         int n=a.size(),cnt=0;
+//         vector<int> in(num,0) , adj[num +1];
+//         queue<int> q;
+//         for(int i=0;i<n;i++) 
+//           adj[a[i][1]].push_back(a[i][0]);//storing directed graph
         
-        for(int i=0;i<n;i++)
-           in[a[i][0]]++;// calculating indegree
+//         for(int i=0;i<n;i++)
+//            in[a[i][0]]++;// calculating indegree
         
         
-        for(int i=0;i<num ;i++){
-            if(in[i]==0)
-              q.push(i); //if indegree==0, push in queue
-        }
+//         for(int i=0;i<num ;i++){
+//             if(in[i]==0)
+//               q.push(i); //if indegree==0, push in queue
+//         }
         
-        while(!q.empty()){
-            int temp=q.front(); q.pop();
-            for(auto &it: adj[temp]){
-                in[it]--;
-                if(in[it]==0) q.push(it);
-            }
-            cnt++;
-        }  
-       return cnt==num;
-    }
+//         while(!q.empty()){
+//             int temp=q.front(); q.pop();
+//             for(auto &it: adj[temp]){
+//                 in[it]--;
+//                 if(in[it]==0) q.push(it);
+//             }
+//             cnt++;
+//         }  
+//        return cnt==num;
+//     }
   
 };
  
