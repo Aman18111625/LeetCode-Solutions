4f223dca93c79f85956bdc660db9c0b85f281587
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-23 00:41:56 +0530

    Added solution - LeetHub

--- Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
+++ Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
@@ -5,21 +5,40 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 	public:
-	int TotalWays(int n)
+	const int mod=1e9+7;
+	int helper(int N,vector<int>&dp)
+    {
+       if(N==0)
+	        return 1;
+	    if(N==1)
+	        return 2;
+	    if(dp[N]!=-1)
+	        return dp[N];
+	    else
+	        return dp[N] = (helper(N-1,dp)%mod+ helper(N-2,dp)%mod)%mod;
+    }
+	int TotalWays(int N)
 	{
-	    // Code here
-	    int mod=1e9+7;
-	    if(n==0) return 1;
-	    if(n==1) return 4;
-	    int a=1,b=2;
-	    long long int ans=0;
-	    for(int i=2;i<=n;i++){
-	        ans=(a+b)%mod;
-	        a=b;
-	        b=ans;
-	    }
+	    vector<int>dp(N+1,-1);
+	    long long int ans = helper(N,dp)%mod;
 	    return (ans*ans)%mod;
 	}
+	
+// 	int TotalWays(int n)
+// 	{
+// 	    // Code here
+// 	    int mod=1e9+7;
+// 	    if(n==0) return 1;
+// 	    if(n==1) return 4;
+// 	    int a=1,b=2;
+// 	    long long int ans=0;
+// 	    for(int i=2;i<=n;i++){
+// 	        ans=(a+b)%mod;
+// 	        a=b;
+// 	        b=ans;
+// 	    }
+// 	    return (ans*ans)%mod;
+// 	}
 };
 
 // { Driver Code Starts.
