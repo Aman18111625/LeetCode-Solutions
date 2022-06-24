114be89e1e1e7d14a2df006992dad7ed8a2f66ac
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-24 12:59:14 +0530

    Time: 62 ms (55.24%), Space: 30.1 MB (37.06%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        priority_queue<int> pq;//Priority queue for storing all the nums in taget in decreasing order
+        long long sum = 0;//for storing total sum
+        for(auto num : target){//adding the num into pq and find total sum
+            pq.push(num);
+            sum+=num;
+        }
+       //iterating untill all elements in pq become 1 (in turn pq.top() will also become 1);
+        while(pq.top() != 1){
+            sum -= pq.top();//removing the greatest element as it was last upadted when converting [1,1,1...] array to target. So we are left with sum of other elements.
+            
+            //when there are elements greater than 1 then sum of other elements can not be 0 or sum can not be greater than top element because sum + x(any number>0) is pq.top().
+            if(sum == 0 || sum > pq.top()) return false;
+          //if we delete all copies of sum from pq.top() we get an old element.
+            int old = pq.top()%sum;
+          //all old elements were > 0 so it can not be 0 unless sum is 1 (This is only possible if array has only 2 elements)
+            if(sum != 1 && old == 0) return false;
+            pq.pop();//deleting greatest element
+            pq.push(old);//Adding old element to restore array.
+            sum += (old);//updating sum
+        }
+      //if all elements are 1
+        return true;
+    }
+};
