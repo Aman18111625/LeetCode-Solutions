4d5f667eea2ba92186fc71c41ca278ebbe358b85
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-24 13:12:38 +0530

    Time: 58 ms (62.24%), Space: 30.1 MB (51.05%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -9,17 +9,14 @@ public:
         }
        //iterating untill all elements in pq become 1 (in turn pq.top() will also become 1);
         while(pq.top() != 1){
-            sum -= pq.top();//removing the greatest element as it was last upadted when converting [1,1,1...] array to target. So we are left with sum of other elements.
-            
-            //when there are elements greater than 1 then sum of other elements can not be 0 or sum can not be greater than top element because sum + x(any number>0) is pq.top().
-            if(sum == 0 || sum > pq.top()) return false;
+            int top=pq.top(); pq.pop();//deleting greatest element
+            sum -= top;//removing the greatest element as it was last upadted when converting [1,1,1...] array to target. So we are left with sum of other elements.
+//when there are elements greater than 1 then sum of other elements can not be 0 or sum can not be greater than top element because sum + x(any number>0) is pq.top().
+            if(sum <= 0 || sum >= top) return false;
           //if we delete all copies of sum from pq.top() we get an old element.
-            int old = pq.top()%sum;
-          //all old elements were > 0 so it can not be 0 unless sum is 1 (This is only possible if array has only 2 elements)
-            if(sum != 1 && old == 0) return false;
-            pq.pop();//deleting greatest element
-            pq.push(old);//Adding old element to restore array.
-            sum += (old);//updating sum
+            top%=sum;
+            top>0?pq.push(top):pq.push(sum);//Adding old element to restore array.
+            sum += (top);//updating sum
         }
       //if all elements are 1
         return true;
