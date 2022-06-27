55ace24c4177375e12c637186b1378c42d58e63f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-06-27 10:56:07 +0530

    Time: 3 ms (78.55%), Space: 8.6 MB (34.08%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+  //Here We need to just find prefix sum of given array
+  //TC:->O(N)
+  //SC:->O(1)(we're modifying the given array,not using extra array) If we're not allowed to modify the given then O(N)
     vector<int> runningSum(vector<int>& nums) {
        int n=nums.size();
        for(int i=1;i<n;i++){
