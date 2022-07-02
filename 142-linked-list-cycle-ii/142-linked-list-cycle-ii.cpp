3c4a10b7d041787cb5294007b077c6acb3bce9ed
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-02 17:36:39 +0530

    Time: 11 ms (66.07%), Space: 7.6 MB (34.50%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -8,29 +8,41 @@
  */
 class Solution {
 public:
+  //Brute-Force Approach
+  //TC:=>O(N)
+  //SC:=>O(N)
+  // node* detectCycle(node* head) {
+  //   unordered_set<node*> st;
+  //   while(head) {
+  //       if(st.find(head) != st.end()) return head;
+  //       st.insert(head);
+  //       head = head->next;
+  //   }
+  //   return NULL;
+  // }
+  
+  //Optimal Approach
+  //TC:=>O(N)
+  //SC:=>O(1)
     ListNode *detectCycle(ListNode *head) {
         if(!head) return head;
         ListNode* slow=head,*fast=head;
         bool found=false;
-        while(slow && fast && fast->next)
-        {
+        //find the collision point
+        while(slow && fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast) 
-            {
+            if(slow==fast) {
                 found=true;
                 break;
             }
         }
         if(!found) return NULL;//there's no loop in the LL.
-        if(slow==head)
-        {
+        if(slow==head){
           while(fast->next!=slow) fast=fast->next;
-        }else if(slow==fast)
-        {
+        }else if(slow==fast){
             slow=head;
-            while(fast->next!=slow->next) 
-            {
+            while(fast->next!=slow->next) {
                 slow=slow->next;
                 fast=fast->next;
             }
