5d25fe5a0050e588d1dbe3258c160f6f669a72ff
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-02 18:20:30 +0530

    Added solution - LeetHub

--- Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
+++ Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
@@ -8,22 +8,12 @@ class Solution{
     long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2)
     {
         // Your code goes here
-        priority_queue<long long>pq;
-        for(int i=0;i<K2;i++) pq.push(A[i]);
-        for(int i=K2;i<N;i++){
-            long long temp=pq.top();
-            if(A[i]<temp) {
-                pq.pop();
-                pq.push(A[i]);
-            }
+        sort(A,A+N);
+        long long sum=0;
+        for(int i=K1;i<K2-1;i++){
+            sum+=A[i];
         }
-        pq.pop();//k2 smallest element out
-        long long ans=0;
-        while(pq.size()>K1){
-            ans+=pq.top();
-            pq.pop();
-        }
-        return ans;
+        return sum;
     }
 };
 
