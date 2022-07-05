fffaacebbb957b8938bbac0f285517ffac9385bc
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-05 11:21:26 +0530

    Time: 3 ms (97.94%), Space: 10.4 MB (35.57%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -4,7 +4,7 @@ public:
          unordered_map<int,int>mpp;
          for(auto num : arr){
              if(mpp.find(2*num)!=mpp.end()) return true;
-             else if(num%2==0 and mpp.find(num/2)!=mpp.end()) return true;
+             else if(num%2==0 && mpp.find(num/2)!=mpp.end()) return true;
              else 
                  mpp[num]++;
          }
