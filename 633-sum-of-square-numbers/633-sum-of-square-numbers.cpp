6fcd6e27c80565b213b556ea96af93fdef3b1bad
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-06 10:32:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.50%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-      if(c<0) return false;
     	long int left=0,right=sqrt(c);
     	while(left<=right){
     		long int cur=left*left+right*right;
