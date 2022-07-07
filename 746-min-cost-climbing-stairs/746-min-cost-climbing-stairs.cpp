0b80f726afdca5839dab0374622c7828bd512379
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-07 14:25:07 +0530

    Time: 4 ms (89.46%), Space: 13.9 MB (28.70%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-    int dp[1001];
-    int helper(int i,vector<int>&cost){
-       if(i>=cost.size()) return 0;
-       if(dp[i]!=-1) return dp[i];
-       int op1=helper(i+1,cost);
-       int op2=helper(i+2,cost);
-       return dp[i]=cost[i]+min(op1,op2);
-    }
-      int minCostClimbingStairs(vector<int>& cost) {
-        memset(dp,-1,sizeof(dp));
-        return  min(helper(0,cost),helper(1,cost));
+    //Recursive+Memoization
+    // int dp[1001];
+    // int helper(int i,vector<int>&cost){
+    //    if(i>=cost.size()) return 0;
+    //    if(dp[i]!=-1) return dp[i];
+    //    int op1=helper(i+1,cost);
+    //    int op2=helper(i+2,cost);
+    //    return dp[i]=cost[i]+min(op1,op2);
+    // }
+    //   int minCostClimbingStairs(vector<int>& cost) {
+    //     memset(dp,-1,sizeof(dp));
+    //     return  min(helper(0,cost),helper(1,cost));
+    // }
+  
+  //Bottom-Up Approach;
+   int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp(n+2);
+        dp[n]=0;
+        dp[n+1]=0;
+        for(int i=n-1;i>=0;i--)
+            dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+        
+        return min(dp[0],dp[1]);
     }
 };
