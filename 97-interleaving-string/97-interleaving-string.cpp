fdb2ae874e73039ceaa6db329f9e57f1e2d77587
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-07 14:03:51 +0530

    Time: 7 ms (57.29%), Space: 6.6 MB (72.32%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,58 @@
 class Solution {
 public:
-    int dp[101][101];
-    bool helper(string &s1,string &s2,string &s3,int i,int j,int k){
-      if(i==s1.size() && j==s2.size() && k==s3.size()) return true;
-      if(dp[i][j]!=-1) return dp[i][j];
-      bool res=false;
-      if(i!=s1.size() && s1[i]==s3[k])
-         res|=helper(s1,s2,s3,i+1,j,k+1);
-      if(j!=s2.size() && s2[j]==s3[k])
-          res|=helper(s1,s2,s3,i,j+1,k+1);
+//Recursive+Memoization
+//TC:=>O(N*M)
+//SC:=>O(N*M)+O(max(N,M))(stack-space)
+//     int dp[101][101];
+//     bool helper(string &s1,string &s2,string &s3,int i,int j,int k){
+//       if(i==s1.size() && j==s2.size() && k==s3.size()) return true;
+//       if(dp[i][j]!=-1) return dp[i][j];
+//       bool res=false;
+//       if(i!=s1.size() && s1[i]==s3[k])
+//          res|=helper(s1,s2,s3,i+1,j,k+1);
+//       if(j!=s2.size() && s2[j]==s3[k])
+//           res|=helper(s1,s2,s3,i,j+1,k+1);
         
-      return dp[i][j]=res;    
-    }
-    bool isInterleave(string s1, string s2, string s3) {
-        int n=s1.size(),m=s2.size();
+//       return dp[i][j]=res;    
+//     }
+//     bool isInterleave(string s1, string s2, string s3) {
+//         int n=s1.size(),m=s2.size();
+//         if(s3.size()==0) return true;
+//         if(n+m!=s3.size()) return false;
+//         memset(dp,-1,sizeof(dp));
+//         return helper(s1,s2,s3,0,0,0);
+//     }
+  
+  //Bottom Approach
+  bool isInterleave(string s1, string s2, string s3) {
+        int n=s1.length();
+        int m=s2.length();
         if(s3.size()==0) return true;
-        if(n+m!=s3.size()) return false;
-        memset(dp,-1,sizeof(dp));
-        return helper(s1,s2,s3,0,0,0);
+        if(n+m!=s3.length())  return 0;
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0&&j==0)
+                    dp[i][j]=true;
+                else if(i==0){
+                    if(s2[j-1]==s3[j-1])
+                        dp[i][j]=dp[i][j-1];
+                }
+                else if(j==0){
+                    if(s1[i-1]==s3[i-1])
+                        dp[i][j]=dp[i-1][j];
+                }
+                else if(s1[i-1]==s3[i+j-1]&&s2[j-1]!=s3[i+j-1])
+                    dp[i][j]=dp[i-1][j];
+                
+                else if(s1[i-1]!=s3[i+j-1]&&s2[j-1]==s3[i+j-1])
+                    dp[i][j]=dp[i][j-1];
+                
+                else if(s1[i-1]==s3[i+j-1]&&s2[j-1]==s3[i+j-1])
+                    dp[i][j]=(dp[i-1][j] || dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
 
