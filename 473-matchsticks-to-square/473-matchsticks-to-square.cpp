eb2a056d660a9afc280ef5e43ebe4931d34ea4f7
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-12 12:15:41 +0530

    Time: 1013 ms (14.36%), Space: 10.1 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -3,11 +3,11 @@ public:
   
    bool helper(vector<int>&matchsticks,vector<int>&vis,int target,int currSum,int ind,int subsets){
      if(subsets==1) return true;//if subsets==1 then we found all the subsets
-     //we found one subset go on to the next one starting from currSum==0
+     //we found one subset whose sum is equal to target go on to the next one starting from currSum==0
      if(currSum==target) return helper(matchsticks,vis,target,0,0,subsets-1);
      for(int j=ind;j<matchsticks.size();j++){
 //If we visited this index already or currSum+matchsticks[j]>target then we can't use it
-       if(vis[j]) continue;
+       if(vis[j] or currSum+matchsticks[j]>target ) continue;
        vis[j]=true;//mark as visited
        if(helper(matchsticks,vis,target,currSum+matchsticks[j],j+1,subsets)) return true;
        vis[j]=false;//mark as unvisited->backtracking->very important step
@@ -20,6 +20,7 @@ public:
         for(auto &match : matchsticks) sum+=match;
         if(n<4 or sum%4) return false;
         vector<int>vis(n,false);
+      //we have to find subset of sum equal to sum/4
         return helper(matchsticks,vis,sum/4,0,0,4);
     }
 };
