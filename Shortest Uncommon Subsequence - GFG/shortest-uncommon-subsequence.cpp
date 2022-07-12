34e1bbe5fea7c584a2e1001da0e371bb7cfb615f
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-12 11:55:49 +0530

    Added solution - LeetHub

---
+++ Shortest Uncommon Subsequence - GFG/shortest-uncommon-subsequence.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int shortestUnSub(string s, string t) {
+        // code here
+        int n=s.size(),m=t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
+        for(int i=1;i<=n;i++) dp[i][0]=1;
+        for(int j=0;j<=m;j++) dp[0][j]=1e4;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int k=j;
+                for(k;k>=0;k--){
+                    if(s[i]==t[k]) break;
+                }
+                if(k<0) dp[i+1][j+1]=1;
+                else 
+                dp[i+1][j+1]=min(dp[i][j+1],1+dp[i][k]);
+            }
+        }
+        return dp[n][m]>=1e4?-1:dp[n][m]; 
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S,T;
+        cin>>S>>T;
+
+        Solution ob;
+        cout << ob.shortestUnSub(S,T) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
