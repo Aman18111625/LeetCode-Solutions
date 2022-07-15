9307cf4a5f315fd1ed8653ab4ff910bb3d5b51a4
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-15 12:28:33 +0530

    Added solution - LeetHub

---
+++ Maximum XOR subarray - GFG/maximum-xor-subarray.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+
+class Solution{   
+public:
+    int maxSubarrayXOR(int N, int arr[]){    
+        //code here
+        if(N==1) return arr[0];
+        int ans=1,max_sum=INT_MIN;
+        for(int i=0;i<N;i++){
+            if(arr[i]==arr[i+1])return arr[i];
+            ans=max(arr[i],arr[i]^ans);
+            max_sum=max(max_sum,ans);
+        }
+        return max_sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        cout << ob.maxSubarrayXOR(N, arr) << endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
