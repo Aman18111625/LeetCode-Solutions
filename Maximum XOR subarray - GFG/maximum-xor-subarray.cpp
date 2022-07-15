1570f884a31ac253e37dd80e3c220ba8e7c1074d
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-15 12:36:55 +0530

    Added solution - LeetHub

--- Maximum XOR subarray - GFG/maximum-xor-subarray.cpp
+++ Maximum XOR subarray - GFG/maximum-xor-subarray.cpp
@@ -7,20 +7,65 @@ using namespace std;
  // } Driver Code Ends
 //User function Template for C++
 
+class Node{
+  public:
+  Node* child[2];
+  int val;
+  Node(){
+      for(int i=0;i<2;i++) child[i]=NULL;
+      val=0;
+  }
+};
 
+class Trie{
+  public:
+  Node* root;
+  Trie(){
+      root=new Node();
+  }
+  
+  void insert(int n){
+      Node* temp=root;
+      for(int i=31;i>=0;i--){
+          int bit=(n>>i)&1;
+          if(temp->child[bit]==NULL){
+              temp->child[bit]=new Node();
+          }
+          temp=temp->child[bit];
+      }
+      temp->val=n;
+  }
+  
+  int findXor(int n){
+      Node* temp=root;
+      for(int i=31;i>=0;i--){
+          int bit=(n>>i)&1;
+          if(temp->child[1-bit]!=NULL){
+              temp=temp->child[1-bit];
+          }else if(temp->child[bit]!=NULL){
+              temp=temp->child[bit];
+          }
+      }
+      return (temp->val)^n;
+  }
+  
+};
 
 class Solution{   
 public:
     int maxSubarrayXOR(int N, int arr[]){    
         //code here
-        if(N==1) return arr[0];
-        int ans=1,max_sum=INT_MIN;
+        vector<int>PreXor(N);
+        PreXor[0]=arr[0];
+        for(int i=1;i<N;i++) PreXor[i]=PreXor[i-1]^arr[i];
+        Trie *trie=new Trie();
+        trie->insert(0);
+        int ans=0;
         for(int i=0;i<N;i++){
-            if(arr[i]==arr[i+1])return arr[i];
-            ans=max(arr[i],arr[i]^ans);
-            max_sum=max(max_sum,ans);
+            trie->insert(PreXor[i]);
+            ans=max(ans,trie->findXor(PreXor[i]));
         }
-        return max_sum;
+        return ans;
     }
 };
 
