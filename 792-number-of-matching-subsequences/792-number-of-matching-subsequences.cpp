9eb547dbfb053c2f59abc0b5230cbb69c93782c3
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-07-20 17:52:50 +0530

    Time: 662 ms (24.90%), Space: 335 MB (18.35%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   //to check whether str1 is sub-sequence of str2 or not
+   bool isSubSequence(string str1,string str2){
+      int j=0,n=str2.size(),m=str1.size();   
+      for(int i=0;i<n && j<m;i++)
+        if(str1[j]==str2[i]) j++;  
+      
+         return j==m;
+     }
+  
+     int numMatchingSubseq(string s, vector<string>& words) {
+        int count=0;
+        unordered_map<string,bool>map;
+        for(int i=0;i<words.size();i++){
+         if(map.find(words[i])!=map.end()){
+          if(map[words[i]]) count++;
+         }
+        else{
+         bool match=isSubSequence(words[i],s);
+         map[words[i]]=match;
+         if(match) count++;
+        }
+      }
+      return count;
+    }
+};
