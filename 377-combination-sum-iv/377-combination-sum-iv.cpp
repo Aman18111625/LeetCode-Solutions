d778d69c7744e48a1908853e56595d80de7fe382
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-08-05 08:49:32 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -12,19 +12,32 @@ public:
     //     }
     //     return ans;
     // }
-  int helper(vector<int>&nums,int target,vector<int>&dp){
-    if(target==0) return 1;
-    if(dp[target]!=-1) return dp[target];
-    dp[target]=0;
-    for(auto num: nums){
-      if(num<=target)
-        dp[target]+=helper(nums,target-num,dp);
-    }
-    return dp[target];
-  }
+
+//Recursive+Memoization
+//TC:=>O(N*T)
+//SC:=>O(N)(stack-space)+O(T)
+//   int helper(vector<int>&nums,int target,vector<int>&dp){
+//     if(target==0) return 1;
+//     if(dp[target]!=-1) return dp[target];
+//     dp[target]=0;
+//     for(auto num: nums){
+//       if(num<=target)
+//         dp[target]+=helper(nums,target-num,dp);
+//     }
+//     return dp[target];
+//   }
     
+//   int combinationSum4(vector<int>& nums, int target) {
+//     vector<int>dp(target+1,-1);
+//     return helper(nums,target,dp);
+//   }
   int combinationSum4(vector<int>& nums, int target) {
-    vector<int>dp(target+1,-1);
-    return helper(nums,target,dp);
+        sort(begin(nums), end(nums));
+        vector<uint> dp(target + 1); dp[0] = 1;
+	     for (int curTarget = 0; curTarget <= target; curTarget++) 
+          for (auto& num : nums) 
+             if (num <= curTarget)  dp[curTarget] += dp[curTarget - num];
+             else break;
+       return dp[target];
   }
 };
