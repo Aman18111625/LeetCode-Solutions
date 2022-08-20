02791f07df1bd64dec8e3ba98d42c2bcce005c28
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-08-20 20:30:07 +0530

    Time: 180 ms (71.43%), Space: 13.8 MB (85.30%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -8,28 +8,24 @@ public:
       //step2 Use queue to store depth wise word 
            queue<string>q;
            q.push(start);//intially push start word in queue
-          int len=start.size();//according to  constraint all the word should be of same size
-          int depth=0;
-        while(!q.empty())
-        {
-          depth++;
-          int size=q.size();
-          for(int i=0;i<size;i++)
-          {
-            string temp=q.front(); q.pop();
-            //now check for each & every character of string temp
-             for(int l=0;l<len;l++)
-             {
+           int len=start.size();//according to  constraint all the word should be of same size
+           int depth=0;
+       while(!q.empty()){
+           depth++;
+           int size=q.size();
+           for(int i=0;i<size;i++){
+             string temp=q.front(); q.pop();
+             //now check for each & every character of string temp
+             for(int l=0;l<len;l++){
                 char curr=temp[l];
-                for(char ch='a';ch<='z';ch++)
-                {
+                for(char ch='a';ch<='z';ch++){
                   temp[l]=ch;//replace from 'a' to 'z' and check if it's equal to end then return depth+1 else check whether it's present in set or not if not then continue else erase from set and push into queue
                   if(temp==end) return depth+1;
                   if(word.find(temp)==word.end()) continue;
                   word.erase(temp);
                   q.push(temp);
                 }
-              temp[l]=curr;//backtrack to curr word  because we have to check all the possibility
+                temp[l]=curr;//backtrack to curr word  because we have to check all the possibility
              }
           }
         }
