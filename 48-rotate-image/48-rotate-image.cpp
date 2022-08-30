164b1fe50fc80e4a812fef821ac810d864fed492
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-08-30 21:34:03 +0530

    Time: 9 ms (5.42%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
   //Approach-1 Brute Force
   //Time-Complexity:=>O(N^2) Space-Complexity:=>O(N^2)
+  //create a dummy matrix of size n*n ,now put first row of matrix into last column..2nd row->2nd last column and so on...
 //     void rotate(vector<vector<int>>& mat) {
 //          int n=mat.size();
 //          vector<vector<int>>ans(n,vector<int>(n,0));
@@ -19,8 +20,11 @@ public:
       
 //     }
   //Approach-2 Optimized Approach
+  //Intuition:=> By observation, we see that the first column of the original matrix is the reverse of the first row of the rotated matrix, so that’s why we transpose the matrix and then reverse each row
   //step-1 Transpose of given matrix so that row will become column and hence column will become row
   //Step-2 Now reverse each row
+  //Time-Complexity=>O(N^2)
+  //Space-Complexity=>O(1)
    void rotate(vector<vector<int>>& mat) {
          int n=mat.size();
          for(int i=0;i<n;i++){
