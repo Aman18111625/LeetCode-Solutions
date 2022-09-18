766727c955eda4e7ec6f3001f3007b35785c02cb
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-09-18 11:53:14 +0530

    Time: 16 ms (89.29%), Space: 19.7 MB (96.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -45,9 +45,10 @@ public:
       //   return ans;
       // }
        
-       //Optimal Approach ->Using Two Pointer
-      //TC:=>O(N)
+        //Optimal Approach ->Using Two Pointer
+       //TC:=>O(N)
       //SC:=>O(1)
+      //Intution:->We need a minimum of leftMax and rightMax.So if we take the case when height[l]<=height[r] we increase l++, so we can surely say that there is a block with height more than height[l] to the right of l. And for the same reason when height[r]<=height[l] we can surely say that there is a block to the left of r which is at least of height height[r]. So by traversing with these cases and using two pointers approach the time complexity can be decreased without using extra space.
         int trap(vector<int>&arr){
           int n=arr.size();
           int left=0,right=n-1;
