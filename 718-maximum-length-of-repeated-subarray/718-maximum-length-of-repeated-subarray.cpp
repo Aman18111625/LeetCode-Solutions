420d2de00efe928de756e8a49599e5d7f2ce81fe
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-09-20 15:03:17 +0530

    Time: 279 ms (86.50%), Space: 11.9 MB (95.52%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -44,23 +44,49 @@ public:
   
   //Bottom-Up Approach:
   //TC:=>O(M*N)
-  //SC:=>O(M*N)
+  //SC:=>O(M*N)  
+   // int findLength(vector<int>& nums1, vector<int>& nums2) {
+   //      if(nums1==nums2) return nums1.size();
+   //      int n=nums1.size(),m=nums2.size();
+   //      int dp[n+1][m+1];
+   //      memset(dp,0,sizeof(dp));
+   //      int ans=0;
+   //      for(int i=1;i<=n;i++){
+   //       for(int j=1;j<=m;j++){
+   //        if(nums1[i-1]==nums2[j-1]){
+   //          dp[i][j]=dp[i-1][j-1]+1;
+   //        }
+   //         ans=max(ans,dp[i][j]);
+   //       }  
+   //      }
+   //      return ans;
+   //  }
+  
+  //Space-Optimization
+  //TC:=>O(N*M)
+  //SC:=>O(M)
   
    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1==nums2) return nums1.size();
-        int n=nums1.size(),m=nums2.size();
-        int dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-        int ans=0;
-        for(int i=1;i<=n;i++){
-         for(int j=1;j<=m;j++){
-          if(nums1[i-1]==nums2[j-1]){
-            dp[i][j]=dp[i-1][j-1]+1;
-          }
-           ans=max(ans,dp[i][j]);
-         }  
+      if(nums1==nums2) return nums1.size();
+      if(nums1.size()<nums2.size()) swap(nums1,nums2); 
+      int n = nums1.size(), m = nums2.size();
+      vector<int>dp(m+1,0);
+      int  ans=0;
+        
+      for(int i=1;i<=n;i++){
+        for(int j=m;j>0;j--){
+         if(nums1[i-1]==nums2[j-1]){
+            dp[j]=1+dp[j-1];
+            if(dp[j]>ans){
+             ans = dp[j];
+             if(ans==m)
+              return ans;
+           }
+         }   
+         else
+            dp[j]=0;
         }
-        return ans;
+      }
+      return ans;
     }
-  
 };
