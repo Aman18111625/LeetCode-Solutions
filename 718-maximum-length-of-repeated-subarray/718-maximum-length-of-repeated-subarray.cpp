5e1497f21229ce89a60cc515e03e95fc4aab69d4
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-09-20 14:56:34 +0530

    Time: 347 ms (77.37%), Space: 16 MB (81.84%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -22,22 +22,45 @@ public:
 //Optimized Approach:=>Recursion+DP
 //TC:=>O(N+M)
 //SC:=>O(N+M)
-   int ans=0;
+
+//    int ans=0;
    
-   int solve(vector<int>& A, vector<int>& B, int i, int j,vector<vector<int>>&dp){
-		if(i >= size(A) || j >= size(B)) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-		solve(A, B, i+1, j,dp), solve(A, B, i, j+1,dp);// recurse to solve for each possible (i, j)
-		int common=(A[i] == B[j])?1+solve(A, B, i+1, j+1,dp):0;//longest common subarray starting at A[i] & B[j]
-		ans = max(ans, common); // update ans to hold max length found till now
-		return dp[i][j]=common;
-	}
+//    int solve(vector<int>& A, vector<int>& B, int i, int j,vector<vector<int>>&dp){
+// 		if(i >= size(A) || j >= size(B)) return 0;
+//     if(dp[i][j]!=-1) return dp[i][j];
+// 		solve(A, B, i+1, j,dp), solve(A, B, i, j+1,dp);// recurse to solve for each possible (i, j)
+// 		int common=(A[i] == B[j])?1+solve(A, B, i+1, j+1,dp):0;//longest common subarray starting at A[i] & B[j]
+// 		ans = max(ans, common); // update ans to hold max length found till now
+// 		return dp[i][j]=common;
+// 	}
   
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-      if(nums1==nums2) return nums1.size();
-      int n=nums1.size(),m=nums2.size();
-      vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-      solve(nums1,nums2,0,0,dp);
-      return ans;
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//       if(nums1==nums2) return nums1.size();
+//       int n=nums1.size(),m=nums2.size();
+//       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+//       solve(nums1,nums2,0,0,dp);
+//       return ans;
+//     }
+  
+  //Bottom-Up Approach:
+  //TC:=>O(M*N)
+  //SC:=>O(M*N)
+  
+   int findLength(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1==nums2) return nums1.size();
+        int n=nums1.size(),m=nums2.size();
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        int ans=0;
+        for(int i=1;i<=n;i++){
+         for(int j=1;j<=m;j++){
+          if(nums1[i-1]==nums2[j-1]){
+            dp[i][j]=dp[i-1][j-1]+1;
+          }
+           ans=max(ans,dp[i][j]);
+         }  
+        }
+        return ans;
     }
+  
 };
