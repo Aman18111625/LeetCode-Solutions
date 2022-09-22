2f5c3015247c1a111ed2cfaf34e9bf3f1919dd97
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-09-22 09:30:14 +0530

    Time: 29 ms (37.38%), Space: 9.7 MB (75.20%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-      vector<string>temp;
-      string curr="";
-      for(auto ch: s){
-        if(ch==' ') {
-          reverse(curr.begin(),curr.end());
-          temp.push_back(curr);
-          curr="";
+    //Brute-Force:=>Using Extra space
+    // string reverseWords(string s) {
+    //   vector<string>temp;
+    //   string curr="";
+    //   for(auto ch: s){
+    //     if(ch==' ') {
+    //       reverse(curr.begin(),curr.end());
+    //       temp.push_back(curr);
+    //       curr="";
+    //     }
+    //     else curr+=ch;
+    //   }
+    //   reverse(curr.begin(),curr.end());
+    //   temp.push_back(curr);
+    //   string ans="";
+    //   for(int i=0;i<temp.size();i++){
+    //     ans+=temp[i];
+    //     if(i!=temp.size()-1) ans+=" ";
+    //   }
+    //   return ans;
+    // }
+   
+  //Optimized Approach:=>Without extra space
+    string reverseWords(string& s){
+        int i = 0;
+        for (int j=0;j<s.size();++j) {
+            if (s[j]==' ') {
+                reverse(s.begin()+i,s.begin()+j);
+                i=j + 1;
+            }
         }
-        else curr+=ch;
-      }
-      reverse(curr.begin(),curr.end());
-      temp.push_back(curr);
-      string ans="";
-      for(int i=0;i<temp.size();i++){
-        ans+=temp[i];
-        if(i!=temp.size()-1) ans+=" ";
-      }
-      return ans;
+        reverse(s.begin() + i, s.end());
+        return s;
     }
 };
