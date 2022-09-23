8cb0d9e7c79998c168058533b864b92c8cb42091
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-09-23 11:57:22 +0530

    Time: 247 ms (18.52%), Space: 67.8 MB (53.50%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    //Brute-Force Approach
+    //TC:=>O(N^2)
+    //SC:=>O(1)
+    // int maxSubArray(vector<int>& nums) {
+    //   int n=nums.size();
+    //   int maxi=INT_MIN;
+    //   for(int i=0;i<n;i++){
+    //     int curr=0;
+    //     for(int j=i;j<n;j++){
+    //       curr+=nums[j];
+    //       maxi=max(maxi,curr);
+    //     }
+    //   }
+    //   return maxi;
+    // }
+    int maxSubArray(vector<int>&nums){
        int curr_sum=0;
        int max_sum=INT_MIN;
        int n=nums.size();
