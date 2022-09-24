a5176961ce1e541e18f742db4122eb75a50b775c
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-09-24 14:38:50 +0530

    Time: 136 ms (95.78%), Space: 93.4 MB (10.79%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
+      //Brute-Force Approach
+      //Time-complexity:->O(N^2)
+      //Space-Complexity:->O(1)
+      // int maxProfit(vector<int>& prices){
+      //   int ans=INT_MIN;
+      //   int n=prices.size();
+      //   for(int i=0;i<n;i++){
+      //     for(int j=i+1;j<n;j++){
+      //       ans=max(ans,prices[j]-prices[i]);
+      //     }
+      //   }
+      //   return ans==INT_MAX?-1:ans;
+      // }
+    
+   //Optimized Approach
+   //Time-Complexity:->O(N)
+  //Space-Complexity:->O(1)
     int maxProfit(vector<int>& prices) {
-        int profit=0,mini=INT_MAX;
-         for(auto &it: prices)
-         {
+         int profit=0,mini=INT_MAX;
+         for(auto &it: prices){
            if(it<mini) mini=it;
            else if(it-mini>profit)
              profit=it-mini;
