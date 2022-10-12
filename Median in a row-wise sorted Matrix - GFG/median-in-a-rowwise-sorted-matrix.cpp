19e4224d5da0a7e220bbba0ceddba2353c75771a
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-10-12 16:05:43 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,52 +1,58 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 
 class Solution{   
 public:
-    //return how many elements are less than or equal to value
-     int helper(vector<int> &arr, int value){
-        int low = 0, high = arr.size()-1;
-        int len = 0;
-        while(low <= high){
-            int mid = (low+high) >> 1;
-            if(arr[mid] < value){
-                len = mid+1;
-                low = mid+1;
-            }
-            else
-                high = mid-1;
+    // int median(vector<vector<int>> &matrix, int r, int c){
+    //     // code here
+    //     vector<int>ans;
+    //     for(int i=0;i<r;i++){
+    //         for(int j=0;j<c;j++){
+    //             ans.push_back(matrix[i][j]);
+    //         }
+    //     }
+    //     sort(ans.begin(),ans.end());
+    //     return ans[(r*c)/2];
+    // }
+    
+    int getCountOfSmaller(vector<int>&arr,int val){
+        int low=0,high=arr.size()-1;
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)>>1;
+            if(arr[mid]<val){
+                low=mid+1;
+                ans=mid+1;
+            }else high=mid-1;
         }
-        return len;
+        return ans;
     }
-    int median(vector<vector<int>> &matrix, int r, int c){
-        // code here 
+    int median(vector<vector<int>>&mat,int r,int c){
         int low=0,high=1e9;
         int ans=0;
         while(low<=high){
-            int mid=(low+high)/2;
-            int cnt=0;
-            for(int i=0;i<r;i++){
-                cnt+=helper(matrix[i],mid);
-            }
-            if(cnt<=(r*c)/2){
+            int mid=(low+high)>>1;
+            int count=0;
+            for(int i=0;i<r;i++)
+             count+=getCountOfSmaller(mat[i],mid);
+            if(count<=(r*c)/2){
                 ans=mid;
                 low=mid+1;
-            }else{
-                high=mid-1;
-            }
+            }else high=mid-1;
         }
         return ans;
     }
 };
+ 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
 {
@@ -64,4 +70,5 @@ int main()
         cout<<obj.median(matrix, r, c)<<endl;        
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
