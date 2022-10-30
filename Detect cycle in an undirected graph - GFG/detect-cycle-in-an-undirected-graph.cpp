a8998cdae63d2d9e1a39785e3ce705d886f75421
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-10-30 22:48:20 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -4,30 +4,46 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+    bool check(int src,int par,vector<int> adj[],vector<int>&vis){
+        vis[src]=1;
+        for(auto it: adj[src]){
+            if(!vis[it]){
+                if(check(it,src,adj,vis)) return true;
+            }else if(par!=it) return true;
+        }
+        return false;
+    }
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
+        // vector<int>vis(V,0);
+        // for(int i=0;i<V;i++){
+        //   if(!vis[i]){
+        //   queue<pair<int,int>>q;
+        //   q.push({i,-1});
+        //   vis[i]=1;
+        //   while(!q.empty()){
+        //         int node=q.front().first;
+        //         int par=q.front().second;
+        //         q.pop();
+        //         for(auto it: adj[node]){
+        //             if(!vis[it]){
+        //                 q.push({it,node});
+        //                 vis[it]=1;
+        //             }else if(par!=it){
+        //                 return true;
+        //             }
+        //         }
+        //     }   
+        //   }
+        // }
+        // return false;
         vector<int>vis(V,0);
         for(int i=0;i<V;i++){
-          if(!vis[i]){
-           queue<pair<int,int>>q;
-           q.push({i,-1});
-           vis[i]=1;
-           while(!q.empty()){
-                int node=q.front().first;
-                int par=q.front().second;
-                q.pop();
-                for(auto it: adj[node]){
-                    if(!vis[it]){
-                        q.push({it,node});
-                        vis[it]=1;
-                    }else if(par!=it){
-                        return true;
-                    }
-                }
-            }   
-          }
+            if(!vis[i]){
+             if(check(i,-1,adj,vis)) return true;
+            }
         }
         return false;
     }
