509c0f0ca7d1d148201d680484bf4f9e3f9d36f0
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-11-05 02:23:08 +0530

    Added solution - LeetHub

--- Find whether path exist - GFG/find-whether-path-exist.cpp
+++ Find whether path exist - GFG/find-whether-path-exist.cpp
@@ -4,62 +4,42 @@ using namespace std;
 
  // } Driver Code Ends
 
-
 class Solution
 {
     public:
-     bool dfs(int x, int y, vector<vector<int>>& v){
-        int n=v.size();
-        if(x<0 || x>=n || y<0 || y>=n || v[x][y]==0) return false;
-        if(v[x][y]==2) return true;
-        v[x][y] = 0;
-        bool ans = dfs(x+1,y,v) || dfs(x-1,y,v) || dfs(x,y+1,v) || dfs(x,y-1,v);
-        return ans;
-     }
-    bool is_Possible(vector<vector<int>>&v){
-        int n = v.size();
-        queue<pair<int,int>>q;
+    
+    bool dfs(vector<vector<int>>&grid,int n,int m,int srci,int srcj,int di,int dj){
+        if(srci<0 or srci>=n or srcj<0 or srcj>=m ) return false;
+        if(grid[srci][srcj]==0 or grid[srci][srcj]==INT_MAX) return false;
+        if(srci==di && srcj==dj) return true;
+        grid[srci][srcj]=INT_MAX;
+        bool res=dfs(grid,n,m,srci+1,srcj,di,dj);
+        res|=dfs(grid,n,m,srci-1,srcj,di,dj);
+        res|=dfs(grid,n,m,srci,srcj+1,di,dj);
+        res|=dfs(grid,n,m,srci,srcj-1,di,dj);
+        return res;
+    } 
+    
+    //Function to find whether a path exists from the source to destination.
+    bool is_Possible(vector<vector<int>>& grid) 
+    {
+        //code here
+        int srci,srcj,di,dj;
+        int n=grid.size(),m=grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(v[i][j]==1){
-                    if(dfs(i,j,v)) return true;
-                    else return false;
+                if(grid[i][j]==1){
+                    srci=i;
+                    srcj=j;
+                }
+                if(grid[i][j]==2){
+                    di=i;
+                    dj=j;
                 }
             }
         }
+        return dfs(grid,n,m,srci,srcj,di,dj);
     }
-    //Approach-1 BFS
-    //Function to find whether a path exists from the source to destination.
-    // bool is_Possible(vector<vector<int>>& grid) 
-    // {
-    //     //code here
-    //     int n=grid.size();
-    //     queue<pair<int,int>>q;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<n;j++){
-    //             if(grid[i][j]==1) {
-    //                 q.push({i,j});
-    //                 grid[i][j]=0;
-    //             }
-    //         }
-    //     }
-    //     int dir[4][2]={{0,1},{0,-1},{1,0},{-1,0}};
-    //     while(!q.empty()){
-    //         int x=q.front().first;
-    //         int y=q.front().second;
-    //          q.pop();
-    //          for(int i=0;i<4;i++){
-    //              int nx=x+dir[i][0];
-    //              int ny=y+dir[i][1];
-    //              if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]!=0){
-    //                  if(grid[nx][ny]==2) return true;
-    //                  q.push({nx,ny});
-    //                  grid[nx][ny]=0;
-    //              }
-    //          }
-    //     }
-    //     return false;
-    // }
 };
 
 // { Driver Code Starts.
