eeac8e35b2c2c0bbe2c396b8e9a417b3788097db
Aman Gupta <79043494+Aman18111625@users.noreply.github.com>
2022-12-21 23:49:49 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -5,33 +5,59 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-    void dfs(int src,vector<int>adj[],vector<int>&vis,stack<int>&st){
-        vis[src]=1;
-        for(auto it: adj[src]){
-           if(!vis[it])
-             dfs(it,adj,vis,st);
-        }
-        st.push(src);
-    }
-	public:
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    vector<int>vis(V,0);
-	    stack<int>st;
-	    for(int i=0;i<V;i++){
-	        if(!vis[i]){
-	            dfs(i,adj,vis,st);
-	        }
-	    }
-	    vector<int>ans;
-	    while(!st.empty()){
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
-	    return ans;
-	}
+    public:
+    
+//     void dfs(int src,vector<int>adj[],vector<int>&vis,stack<int>&st){
+//         vis[src]=1;
+//         for(auto it: adj[src]){
+//           if(!vis[it])
+//              dfs(it,adj,vis,st);
+//         }
+//         st.push(src);
+//     }
+
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    vector<int>vis(V,0);
+// 	    stack<int>st;
+// 	    for(int i=0;i<V;i++){
+// 	        if(!vis[i]){
+// 	            dfs(i,adj,vis,st);
+// 	        }
+// 	    }
+// 	    vector<int>ans;
+// 	    while(!st.empty()){
+// 	        ans.push_back(st.top());
+// 	        st.pop();
+// 	    }
+// 	    return ans;
+// 	}
+  vector<int> topoSort(int V,vector<int> adj[]){
+      vector<int>ans;
+      vector<int>indegree(V,0);
+      for(int i=0;i<V;i++){
+          for(auto it: adj[i]){
+              indegree[it]++;
+          }
+      }
+      
+      queue<int>q;
+      for(int i=0;i<V;i++){
+          if(indegree[i]==0) q.push(i);
+      }
+      while(!q.empty()){
+          int node=q.front();
+          q.pop();
+          ans.push_back(node);
+          for(auto it: adj[node]){
+              indegree[it]--;
+              if(indegree[it]==0) q.push(it);
+          }
+      }
+      return ans;
+  }
 };
 
 //{ Driver Code Starts.
